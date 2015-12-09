530b6fe33940178aa6e270731c821f5162aa414c
d34d <clark@cyngn.com>
2015-12-09 10:38:18 -0800

    LockPatternUtils: Make settings getter and setters protected
    This patch will allow us to extend LockPatternUtils in lineage-sdk
    and have access to getting and setting lock settings, and minimize
    code changes to the original LockPatternUtils class.
    
    Change-Id: I3417f64db1f05cffab47328df574f23dd20776d5

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -1197,7 +1197,8 @@ public class LockPatternUtils {
         return lockoutEndTime;
     }
 
-    private boolean getBoolean(String secureSettingKey, boolean defaultValue, int userId) {
+    /** @hide */
+    protected boolean getBoolean(String secureSettingKey, boolean defaultValue, int userId) {
         try {
             return getLockSettings().getBoolean(secureSettingKey, defaultValue, userId);
         } catch (RemoteException re) {
@@ -1205,7 +1206,8 @@ public class LockPatternUtils {
         }
     }
 
-    private void setBoolean(String secureSettingKey, boolean enabled, int userId) {
+    /** @hide */
+    protected void setBoolean(String secureSettingKey, boolean enabled, int userId) {
         try {
             getLockSettings().setBoolean(secureSettingKey, enabled, userId);
         } catch (RemoteException re) {
@@ -1214,7 +1216,8 @@ public class LockPatternUtils {
         }
     }
 
-    private long getLong(String secureSettingKey, long defaultValue, int userHandle) {
+    /** @hide */
+    protected long getLong(String secureSettingKey, long defaultValue, int userHandle) {
         try {
             return getLockSettings().getLong(secureSettingKey, defaultValue, userHandle);
         } catch (RemoteException re) {
@@ -1222,8 +1225,9 @@ public class LockPatternUtils {
         }
     }
 
+    /** @hide */
     @UnsupportedAppUsage
-    private void setLong(String secureSettingKey, long value, int userHandle) {
+    protected void setLong(String secureSettingKey, long value, int userHandle) {
         try {
             getLockSettings().setLong(secureSettingKey, value, userHandle);
         } catch (RemoteException re) {
@@ -1232,8 +1236,9 @@ public class LockPatternUtils {
         }
     }
 
+    /** @hide */
     @UnsupportedAppUsage
-    private String getString(String secureSettingKey, int userHandle) {
+    protected String getString(String secureSettingKey, int userHandle) {
         try {
             return getLockSettings().getString(secureSettingKey, null, userHandle);
         } catch (RemoteException re) {
@@ -1241,8 +1246,9 @@ public class LockPatternUtils {
         }
     }
 
+    /** @hide */
     @UnsupportedAppUsage
-    private void setString(String secureSettingKey, String value, int userHandle) {
+    protected void setString(String secureSettingKey, String value, int userHandle) {
         try {
             getLockSettings().setString(secureSettingKey, value, userHandle);
         } catch (RemoteException re) {
