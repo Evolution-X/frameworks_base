bd87de841b235b1e214c86432f218bb234bc21f0
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    SystemUI: Add ability to toggle bluetooth battery level [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6489,6 +6489,12 @@ public final class Settings {
         public static final String PPU_SPOOF_BUILD_GMS_ARRAY = "ppu_spoof_build_gms_array";
 
         /**
+         * Enable/disable Bluetooth Battery bar
+         * @hide
+         */
+        public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
