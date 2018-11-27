169836cdcab17b64cf84633aa0979fa80141aec2
Daniel Koman <dankoman30@gmail.com>
2018-11-27 15:31:39 -0700

    Add kill button to notification guts [1/2]
    Change-Id: Id2c2f3259ae2ff45ab28288375bc14148a0e9f1e
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7587,6 +7587,13 @@ public final class Settings {
         public static final String QS_TILE_HAPTIC = "qs_tile_haptic";
 
         /**
+         * Whether to show the kill app button in notification guts
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
+                "notification_guts_kill_app_button";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
