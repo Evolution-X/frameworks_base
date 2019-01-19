f43f537ca99f7c896279787e648738c024629d94
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    SystemUI: [SQUASHED] Ambient display show battery & cpu temperature [1/2]
    - Allow to hide battery in doze
    - Add a method to determine CPU temperature, battery temp
    
    Co-authored-by: Aston-Martinn <advaithbhat9@gmail.com>
    Co-authored-by: eyosen <abittin@gmail.com>
    Change-Id: I2c55f607a9ad1a9693618d5f23ff65a2a203efa1
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/com/android/internal/util/evolution/ThemeUtils.java
+++ core/java/com/android/internal/util/evolution/ThemeUtils.java
@@ -22,6 +22,8 @@ import android.util.PathParser;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
 import android.content.pm.PackageManager;
@@ -38,6 +40,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.PathShape;
 import android.net.Uri;
+import android.os.BatteryManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
@@ -47,6 +50,12 @@ import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -230,5 +239,53 @@ public class ThemeUtils {
     public boolean isDefaultOverlay(String category) {
         return getOverlayPackagesForCategory(category).stream()
                .noneMatch(pkg -> isOverlayEnabled(pkg));
+    }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
+
+    public static String getCPUTemp(Context context) {
+        String value;
+        if (fileExists(context.getResources().getString(
+            com.android.internal.R.string.config_cpu_temp_path))) {
+               value = readOneLine(context.getResources().getString(
+                  com.android.internal.R.string.config_cpu_temp_path));
+        } else {
+            value = "Error";
+        }
+        int cpuTempMultiplier = context.getResources().getInteger(
+                com.android.internal.R.integer.config_sysCPUTempMultiplier);
+        return value == "Error" ? "N/A" : String.format("%s", Integer.parseInt(value) / cpuTempMultiplier) + "°C";
+    }
+    public static boolean fileExists(String filename) {
+        if (filename == null) {
+            return false;
+        }
+        return new File(filename).exists();
+    }
+    public static String readOneLine(String fname) {
+        BufferedReader br;
+        String line = null;
+        try {
+            br = new BufferedReader(new FileReader(fname), 512);
+            try {
+                line = br.readLine();
+            } finally {
+                br.close();
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return line;
     }
 }
