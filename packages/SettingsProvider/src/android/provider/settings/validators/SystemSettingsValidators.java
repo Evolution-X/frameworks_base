547f930d6bdf04e1d08e4aa076abe0935876a40d
Danny Baumann <dannybaumann@web.de>
2019-09-18 08:58:56 +0200

    Add back increasing ring feature (3/3)
    Change-Id: I1d5173fb8134400e5fa72c19181f34e1a5ec4e0d

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -269,5 +269,8 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.PREFERRED_REGION, ANY_STRING_VALIDATOR);
         VALIDATORS.put(System.SWIPE_TO_SCREENSHOT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.INCREASING_RING, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.INCREASING_RING_START_VOLUME, new InclusiveFloatRangeValidator(0, 1));
+        VALIDATORS.put(System.INCREASING_RING_RAMP_UP_TIME, new InclusiveFloatRangeValidator(5, 60));
     }
 }
