4b129495387b4184357410e05dd1faa1fed6d8bb
Pranav Vashi <neobuddy89@gmail.com>
2020-04-16 01:38:35 +0530

    SettingsProvider: Resolve package name that requires permission
    * Google is confused between developer and magician. It assumed that developer will
      understand which app is complaining about not having permission using magic wand.
    
    * Bonus after this patch - it turned out that Google itself required permission:
    
    04-16 01:35:40.293 W/anzv    (3239): java.lang.SecurityException: Permission denial: com.google.android.gms writing to settings requires:android.permission.WRITE_DEVICE_CONFIG
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    AmeChanRain: Adapt for Android U
    
    Change-Id: I8fb2be4a48de31c45076ce59d675c15ac6e5f998

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -2541,7 +2541,7 @@ public class SettingsProvider extends ContentProvider {
                 return;
             }
         }
-        throw new SecurityException("Permission denial, must have one of: "
+        throw new SecurityException("Permission denial: " + resolveCallingPackage() + " must have one of: "
             + Arrays.toString(permissions));
     }
 
