0059db50c32742f1065cc239985180166bea577d
LuK1337 <priv.luk@gmail.com>
2020-10-29 12:46:05 +0100

    Allow lineage resources package to be overlayed by RRO packages
    Change-Id: I152ef9a87b16338e14af508e0f1f42830688c5ea

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -1049,7 +1049,8 @@ final class OverlayManagerServiceImpl {
             OverlayInfo info) {
         // Idmaps for immutable RROs targeting "android", i.e. framework-res.apk, are created at
         // boot time in OverlayConfig.createImmutableFrameworkIdmapsInZygote().
-        return targetPackage != null && !("android".equals(info.getTargetPackageName())
+        return targetPackage != null && !(("android".equals(info.getTargetPackageName()) ||
+                                           "lineageos.platform".equals(info.getTargetPackageName()))
                 && !isPackageConfiguredMutable(overlayPackage));
     }
 
