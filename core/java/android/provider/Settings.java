d6acef99e1cc6df2a6fde60d131bc2ec772937c2
maxwen <max.weninger@gmail.com>
2020-11-11 16:04:30 +0100

    Allow changing back gesture height [1/2]
    * @neobuddy89: Simplify and cleanup!
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6873,6 +6873,9 @@ public final class Settings {
         @Readable
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
