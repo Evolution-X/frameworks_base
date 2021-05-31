0b0fb2835cb7d3635f099021b794d6d463c44ae5
Anushek Prasal <anushekprasal@gmail.com>
2021-05-31 16:44:52 +0530

    base: Introduce automated Sleep Mode [1/2]
    [cjh1249131356]:
    - Adapt to S
    - Dropped AOD toggle in favor of scheduled AOD feature
    - Dropped Aggressive battery toggle
    
    [joeyhuab]
    * Adapt to A13 QPR2 after b8d6c0a.
    * Drop Location toggle for A14 QPR2.
    
    Change-Id: I85fc36e6db7ceeca1a1031f71f3882a776864814
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.RefreshRateTile
 import com.android.systemui.qs.tiles.ScreenshotTile
+import com.android.systemui.qs.tiles.SleepModeTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
@@ -159,6 +160,12 @@ interface LineageModule {
     @StringKey(ScreenshotTile.TILE_SPEC)
     fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
 
+    /** Inject SleepModeTile into tileMap in QSModule */
+    @Binds  
+    @IntoMap
+    @StringKey(SleepModeTile.TILE_SPEC)
+    fun bindSleepModeTile(sleepModeTile: SleepModeTile): QSTileImpl<*>
+
     /** Inject SoundTile into tileMap in QSModule */
     @Binds
     @IntoMap
@@ -224,6 +231,7 @@ interface LineageModule {
         const val READING_MODE_TILE_SPEC = "reading_mode"
         const val REFRESH_RATE_TILE_SPEC = "refresh_rate"
         const val SCREENSHOT_TILE_SPEC = "screenshot"
+        const val SLEEP_MODE_TILE_SPEC = "sleep_mode"
         const val SOUND_TILE_SPEC = "sound"
         const val SYNC_TILE_SPEC = "sync"
         const val USB_TETHER_TILE_SPEC = "usb_tether"
@@ -350,6 +358,21 @@ interface LineageModule {
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.DISPLAY,
+            )
+
+        @Provides
+        @IntoMap
+        @StringKey(SLEEP_MODE_TILE_SPEC)
+        fun provideSleepModeTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(SLEEP_MODE_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_sleep,
+                        labelRes = R.string.quick_settings_sleep_mode_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES,
             )
 
         @Provides
