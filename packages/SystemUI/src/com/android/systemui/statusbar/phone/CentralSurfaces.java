eac673dffae519f30071cfc7dd8dd96ae537a69a
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: add API for disabling gestural navigation
    [jhonboy121]: adapted to A12L and A13
    [neobuddy89]: updated for A14
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
@@ -359,4 +359,6 @@ public interface CentralSurfaces extends Dumpable, LifecycleOwner {
     void updateDismissAllVisibility(boolean visible);
 
     void updateDismissAllButton();
+
+    void setBlockedGesturalNavigation(boolean blocked);
 }
