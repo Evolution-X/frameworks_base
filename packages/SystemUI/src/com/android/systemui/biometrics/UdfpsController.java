2fce46a9aac2cf422c3be89058ad9661d38d34a8
trpsl <trpsl755@gmail.com>
2021-12-03 23:12:32 +0100

    udfps: Allow devices to pulse onFingerDown instead of waking the screen
    Change-Id: If31e9b4c9f1454c22303d8ca0f7d6cbec3786ebf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -47,6 +47,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.util.Log;
@@ -348,8 +349,13 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     return;
                 }
                 if (vendorCode == mUdfpsVendorCode) {
-                    mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
-                            PowerManager.WAKE_REASON_GESTURE, TAG);
+                    if (mContext.getResources().getBoolean(R.bool.config_pulseOnFingerDown)) {
+                        mContext.sendBroadcastAsUser(new Intent(PULSE_ACTION),
+                                new UserHandle(UserHandle.USER_CURRENT));
+                    } else {
+                        mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
+                                PowerManager.WAKE_REASON_GESTURE, TAG);
+                    }
                     onAodInterrupt(0, 0, 0, 0);
                 }
             }
