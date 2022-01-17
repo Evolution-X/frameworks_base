bd14908b02e141b0e4919c6573d3f40cfea9e46b
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    SystemUI: Allow toggling privacy indicators [1/2]
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/events/SystemEventCoordinator.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/events/SystemEventCoordinator.kt
@@ -19,7 +19,6 @@ package com.android.systemui.statusbar.events
 import android.annotation.IntRange
 import android.content.Context
 import android.location.flags.Flags.locationIndicatorsAnimation
-import android.location.flags.Flags.locationIndicatorsEnabled
 import android.provider.DeviceConfig
 import android.provider.DeviceConfig.NAMESPACE_PRIVACY
 import com.android.internal.annotations.VisibleForTesting
@@ -195,9 +194,7 @@ constructor(
                             // For location-only, we show an animation if the flag is enabled. The
                             // 10-minute debounce is handled in filterOutExemptItems.
                             val shouldAnimateLocation =
-                                hasOnlyLocationItems &&
-                                    locationIndicatorsEnabled() &&
-                                    locationIndicatorsAnimation()
+                                hasOnlyLocationItems && privacyController.locationAvailable
 
                             isChipAnimationEnabled() &&
                                 (shouldAnimateCameraMic || shouldAnimateLocation)
@@ -209,7 +206,7 @@ constructor(
                     currentPrivacyItems.forEach {
                         if (
                             it.privacyType == PrivacyType.TYPE_LOCATION &&
-                                locationIndicatorsEnabled()
+                                privacyController.locationAvailable
                         ) {
                             appLastLocationUseTime[it.application.packageName] = now
                         }
@@ -226,7 +223,7 @@ constructor(
             private fun filterOutExemptItems(items: List<PrivacyItem>): List<PrivacyItem> {
                 val now = systemClock.elapsedRealtime()
 
-                val locationFlagEnabled = locationIndicatorsEnabled()
+                val locationFlagEnabled = privacyController.locationAvailable
 
                 // First, filter out camera/mic exemptions
                 val afterCameraMicFilter =
