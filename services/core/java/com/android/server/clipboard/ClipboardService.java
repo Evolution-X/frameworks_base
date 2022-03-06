e530b6083f1d4144fa874272585e9c87824b4c9e
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-03-06 21:29:46 +0530

    services: Whitelist Nfc Service from clipboard toasts

--- services/core/java/com/android/server/clipboard/ClipboardService.java
+++ services/core/java/com/android/server/clipboard/ClipboardService.java
@@ -1493,6 +1493,7 @@ public class ClipboardService extends SystemService {
     @GuardedBy("mLock")
     private void showAccessNotificationLocked(String callingPackage, int uid, @UserIdInt int userId,
             Clipboard clipboard, int accessDeviceId) {
+        String nfcService = "com.android.nfc";
         if (clipboard.primaryClip == null) {
             return;
         }
@@ -1503,6 +1504,9 @@ public class ClipboardService extends SystemService {
         if (Settings.Secure.getIntForUser(getContext().getContentResolver(),
                 Settings.Secure.CLIPBOARD_SHOW_ACCESS_NOTIFICATIONS,
                 (mShowAccessNotifications ? 1 : 0), userId) == 0) {
+            return;
+        }
+        if (callingPackage.equals(nfcService)) {
             return;
         }
         // Don't notify if the app accessing the clipboard is the same as the current owner.
