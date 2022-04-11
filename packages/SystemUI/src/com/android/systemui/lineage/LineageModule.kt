ed9b2fed0507381cd56581460b75d93d5ba0c302
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-11 19:41:45 +0800

    SystemUI: Introduce preferred network tile
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.LocaleTile
 import com.android.systemui.qs.tiles.NfcTile
 import com.android.systemui.qs.tiles.OnTheGoTile
 import com.android.systemui.qs.tiles.PowerShareTile
+import com.android.systemui.qs.tiles.PreferredNetworkTile
 import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.SoundTile
@@ -106,6 +107,12 @@ interface LineageModule {
     @IntoMap
     @StringKey(PowerShareTile.TILE_SPEC)
     fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
+
+    /** Inject PreferredNetworkTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PreferredNetworkTile.TILE_SPEC)
+    fun bindPreferredNetworkTile(preferredNetworkTile: PreferredNetworkTile): QSTileImpl<*>
 
     /** Inject ProfilesTile into tileMap in QSModule */
     @Binds
@@ -260,6 +267,21 @@ interface LineageModule {
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.ACCESSIBILITY
+            )
+        }
+
+        @Provides
+        @IntoMap
+        @StringKey(PreferredNetworkTile.TILE_SPEC)
+        fun providePreferredNetworkTile(uiEventLogger: QsEventLogger): QSTileConfig {
+            return QSTileConfig(
+                tileSpec = TileSpec.create(PreferredNetworkTile.TILE_SPEC),
+                uiConfig = QSTileUIConfig.Resource(
+                    iconRes = R.drawable.ic_preferred_network,
+                    labelRes = R.string.quick_settings_preferred_network_label
+                ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.CONNECTIVITY
             )
         }
     }
