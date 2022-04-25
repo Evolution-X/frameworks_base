94263fd6c47fd3437ea256597bde01727367ab34
Dmitrii <bankersenator@gmail.com>
2022-04-25 11:36:44 +0000

    fwb: stop modem spam
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -13117,7 +13117,7 @@ public class TelephonyManager {
         }
 
         public @ModemActivityInfoError int getErrorCode() {
-            return mErrorCode;
+            return 0;
         }
 
         @Override
