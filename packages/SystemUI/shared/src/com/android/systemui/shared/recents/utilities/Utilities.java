af8219ba02d868a2a48d56b52b45a90d0bd561cf
LuK1337 <priv.luk@gmail.com>
2022-05-14 10:27:45 +0200

    SystemUI: Make isLargeScreen() use correct API for getting window metrics
    getCurrentWindowMetrics() will return invalid size in case current
    window is half size, e.g. in Settings.
    
    Change-Id: I958d5a4532fec47b137f180fe1e18bd9e1f06b92

--- packages/SystemUI/shared/src/com/android/systemui/shared/recents/utilities/Utilities.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/recents/utilities/Utilities.java
@@ -159,7 +159,7 @@ public class Utilities {
 
     /** @return whether or not {@code context} represents that of a large screen device or not */
     public static boolean isLargeScreen(WindowManager windowManager, Resources resources) {
-        final Rect bounds = windowManager.getCurrentWindowMetrics().getBounds();
+        final Rect bounds = windowManager.getMaximumWindowMetrics().getBounds();
 
         float smallestWidth = dpiFromPx(Math.min(bounds.width(), bounds.height()),
                 resources.getConfiguration().densityDpi);
