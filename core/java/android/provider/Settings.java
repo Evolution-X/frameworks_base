e03b76f09c453515c5cd4eb2c48ecd01755cdca1
HELLBOY017 <abhaygill017@gmail.com>
2022-06-12 17:42:59 +0530

    base: Add switch for small landscape notifications [1/2]
    Change-Id: Iab534b2f66eba2f162654c614867a3ef63a1563f
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7252,6 +7252,12 @@ public final class Settings {
         public static final String QS_BRIGHTNESS_SLIDER_SHAPE = "qs_brightness_slider_shape";
 
         /**
+         * Whether to enable small size landcape notifications or not
+         * @hide
+         */
+        public static final String SMALL_LANDSCAPE_NOTIFICATION = "small_landscape_notification";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -59,6 +59,7 @@ import android.graphics.Shader;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.IndentingPrintWriter;
@@ -994,7 +995,14 @@ public class NotificationStackScrollLayout
 
         final int innerWidth = viewWidth - mMinimumPaddings * 2;
         final int qsTileWidth = (innerWidth - mQsTilePadding * 3) / 4;
+        boolean smallNotification = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SMALL_LANDSCAPE_NOTIFICATION, 0, UserHandle.USER_CURRENT) == 1;
+        if (smallNotification) {
+        mSidePaddings = mMinimumPaddings + qsTileWidth + mQsTilePadding;
+        }
+        else {
         mSidePaddings = mMinimumPaddings;
+        }
     }
 
     void updateCornerRadius() {
