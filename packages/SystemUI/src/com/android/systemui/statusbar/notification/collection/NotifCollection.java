cafa2697a25822b3842622f4145155ff674da79c
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-07-29 17:12:24 +0800

    [SQUASHED] base: Add status bar lyric for Android 17
    * This is a squash of:
    
    * base: Add status bar lyric [1/3]
        Change-Id: I5c49187b0752286ee00a60d961203c0c4ec0d555
    
    * fixup! base: Add status bar lyric [1/3]
         * java.lang.SecurityException: Settings key: <status_bar_show_lyric> is
           not readable. From S+, settings keys annotated with @hide are
           restricted to system_server and system apps only, unless they are
           annotated with @Readable.
    
        Change-Id: Ia0c209f3ddd5bbe1be676e172f408b3faf8cfe81
    
    * base: Adapt StatusBar Lyric for Miku UI Udon
    
    * fixup! [SQUASHED] base: Add status bar lyric for Android 14
        Change-Id: I68e9c9d3b17cb506cd87ef764d9fababa13f8357
    
    * Adapt statusbar lyric for Android V
        Change-Id: I70f7447c3fc2f277466c25e0233ce2dfc0419730
    
    * statusbar: BP1A fixup for status bar lyric
        Change-Id: I34f5d95bc6d2f7ff9a0f6f327d304d43587bd398
    
    * Status bar lyrics: Fix bug where some lyric would be wrongly ignored
        FLAG_ONLY_UPDATE_TICKER may not always be set by the app because it will prevent update of notification content in Flyme OS, follow Flyme OS behaviour to classify what lyric is
    
    * SystemUI: Adapt status bar lyric to modern pipeline
        Migrate lyric feature from legacy CollapsedStatusBarFragment to
        modern HomeStatusBarViewBinder pipeline.
    
    * SystemUI: Adapt status bar lyric to Android 17
        * and also Migrate to kt
    
    Co-authored-by: LynnrinChan <wuxiaofei31@gmail.com>
    Co-authored-by: AmeChanRain <downloadbot007@gmail.com>
    Co-authored-by: Nick <nift4@protonmail.com>
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Co-authored-by: Arman-ATI <altafiarman5@gmail.com>
    Change-Id: I55d8299268a633de9b8b9fcb1d9ebab7dbe901fc

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/collection/NotifCollection.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/collection/NotifCollection.java
@@ -585,6 +585,10 @@ public class NotifCollection implements Dumpable, PipelineDumpable {
 
             mLogger.logNotifUpdated(entry);
             mEventQueue.add(new EntryUpdatedEvent(entry, source));
+            // update ticker only, LyricController will handle it
+            if ((entry.getSbn().getNotification().flags & Notification.FLAG_ONLY_UPDATE_TICKER) != 0) {
+                return;
+            }
         }
     }
 
---
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LyricControllerModern.kt
@@ -0,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: DerpFest AOSP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.systemui.statusbar.phone
+
+import android.content.Context
+import android.database.ContentObserver
+import android.os.Handler
+import android.os.Looper
+import android.os.UserHandle
+import android.provider.Settings
+import android.view.View
+
+/**
+ * Standalone lyric controller for modern status bar pipeline.
+ * Manages lyric display independently without requiring CollapsedStatusBarFragment.
+ */
+class LyricControllerModern(
+    private val context: Context,
+    private val lyricContainer: View,
+    private val leftSideView: View?,
+    private val centeredAreaView: View?,
+) : LyricViewController(context, lyricContainer.rootView) {
+
+    private var settingsObserver: ContentObserver? = null
+    private var isNotificationIconsVisible = true
+    private var hideForHun = false
+    private var hasOngoingActivity = false
+
+    override fun showLyricView(animate: Boolean) {
+        // Only show if all conditions are met
+        if (!isNotificationIconsVisible || hideForHun || hasOngoingActivity || !isLyricStarted()) {
+            hideLyricView(animate)
+            return
+        }
+        lyricContainer.visibility = View.VISIBLE
+        // Hide left side and centered area when showing lyrics
+        leftSideView?.visibility = View.GONE
+        centeredAreaView?.visibility = View.GONE
+    }
+
+    override fun hideLyricView(animate: Boolean) {
+        lyricContainer.visibility = View.GONE
+        // Restore left side and centered area visibility when hiding lyrics
+        // These views are managed by the view model, but we need to show them
+        // when lyrics are hidden to restore the normal status bar layout
+        leftSideView?.visibility = View.VISIBLE
+        centeredAreaView?.visibility = View.VISIBLE
+    }
+
+    fun attach() {
+        if (settingsObserver != null) {
+            return
+        }
+
+        settingsObserver = object : ContentObserver(Handler(Looper.getMainLooper())) {
+            override fun onChange(selfChange: Boolean) {
+                val enabled = Settings.Secure.getIntForUser(
+                    context.contentResolver,
+                    Settings.Secure.STATUS_BAR_SHOW_LYRIC,
+                    0,
+                    UserHandle.USER_CURRENT
+                ) != 0
+                setEnabled(enabled)
+            }
+        }
+
+        context.contentResolver.registerContentObserver(
+            Settings.Secure.getUriFor(Settings.Secure.STATUS_BAR_SHOW_LYRIC),
+            false,
+            settingsObserver!!,
+            UserHandle.USER_ALL
+        )
+
+        // Initialize enabled state
+        val enabled = Settings.Secure.getIntForUser(
+            context.contentResolver,
+            Settings.Secure.STATUS_BAR_SHOW_LYRIC,
+            0,
+            UserHandle.USER_CURRENT
+        ) != 0
+        setEnabled(enabled)
+    }
+
+    fun detach() {
+        settingsObserver?.let {
+            context.contentResolver.unregisterContentObserver(it)
+            settingsObserver = null
+        }
+    }
+
+    fun updateVisibility(
+        isNotificationIconsVisible: Boolean,
+        hideForHun: Boolean,
+        hasOngoingActivity: Boolean
+    ) {
+        this.isNotificationIconsVisible = isNotificationIconsVisible
+        this.hideForHun = hideForHun
+        this.hasOngoingActivity = hasOngoingActivity
+
+        if (isLyricStarted()) {
+            showLyricView(true)
+        } else {
+            hideLyricView(true)
+        }
+    }
+}
