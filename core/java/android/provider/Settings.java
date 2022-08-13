a7cfd7f6963a11d6be7913d1f87869a1007411d7
Pranav Vashi <neobuddy89@gmail.com>
2022-08-14 07:07:53 +0800

    SystemUI: Statusbar clock background chip [1/2]
    Original commit in Clock.java by
    StarkDroid <gamerprince.exp@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7282,6 +7282,14 @@ public final class Settings {
         public static final String STATUSBAR_EXTRA_PADDING_END = "statusbar_extra_padding_end";
 
         /**
+         * Statusbar clock background
+         * 0 - hide accented chip  (default)
+         * 1 - show accented chip
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_CHIP = "statusbar_clock_chip";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
