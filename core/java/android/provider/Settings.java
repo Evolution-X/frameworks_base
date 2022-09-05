2dff3444e09c12e15dea46d76e77bc169d1b4e76
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-09-05 04:14:52 +0000

    base: support per-app volume [2/3]
    [ghostrider-reborn]
    - Use app volume icon by amartya (@ yukiscape)
    
    Dhina17:
    - VolumeDialog: Fix per-app volume on secondary users
    
    nurkeinneid:
    - Convert app volume button to Compose/Kotlin
    
    neobuddy89:
    - Drop legacy bits
    
    Change-Id: I674cda1d7463b3603ccaac673178aeead4093619
    Co-authored-by: yukiscape <garikapatiamartya@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7633,6 +7633,12 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -23566,6 +23572,12 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_VOLUME =
                 "android.settings.panel.action.VOLUME";
+
+        /**
+         * @hide
+         */
+        public static final String ACTION_APP_VOLUME =
+                "android.settings.panel.action.APP_VOLUME";
     }
 
     /**
