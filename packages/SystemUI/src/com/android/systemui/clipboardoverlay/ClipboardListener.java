63a7ca4401aa016b7a144b16e91013354bea79ca
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-24 00:24:27 +0530

    SystemUI: Allow disabling clipboard overlay [1/2]
    Change-Id: Ia755ca9021468a53c8f17413ca3faa787b00ceea

--- packages/SystemUI/src/com/android/systemui/clipboardoverlay/ClipboardListener.java
+++ packages/SystemUI/src/com/android/systemui/clipboardoverlay/ClipboardListener.java
@@ -91,6 +91,10 @@ public class ClipboardListener implements
 
     @Override
     public void onPrimaryClipChanged() {
+        if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.SHOW_CLIPBOARD_OVERLAY, 1, UserHandle.USER_CURRENT) == 0) {
+            return;
+        }
         if (!mClipboardManager.hasPrimaryClip()) {
             return;
         }
