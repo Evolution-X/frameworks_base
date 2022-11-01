a46a3b7c2daa3f808cb9e5678bf1154aae8893ac
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-01 16:35:04 +0530

    SystemUI: CentralSurfacesImpl: Fix build after d1 merge [1/2]
    Add back mMainHandler since it's used by our settings observer.
    
    Change-Id: Id3838b8943fac0451c7e5e69d4dbc6c9e048e205

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -560,6 +560,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         }
     }
 
+    private Handler mMainHandler;
     private final DelayableExecutor mMainExecutor;
 
     private int mInteractingWindows;
@@ -744,6 +745,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             LockscreenShadeTransitionController lockscreenShadeTransitionController,
             FeatureFlags featureFlags,
             KeyguardUnlockAnimationController keyguardUnlockAnimationController,
+            @Main Handler mainHandler,
             @Main DelayableExecutor delayableExecutor,
             @Main MessageRouter messageRouter,
             WallpaperManager wallpaperManager,
@@ -849,6 +851,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mFeatureFlags = featureFlags;
         mIsShortcutListSearchEnabled = featureFlags.isEnabled(Flags.SHORTCUT_LIST_SEARCH_LAYOUT);
         mKeyguardUnlockAnimationController = keyguardUnlockAnimationController;
+        mMainHandler = mainHandler;
         mMainExecutor = delayableExecutor;
         mMessageRouter = messageRouter;
         mWallpaperManager = wallpaperManager;
