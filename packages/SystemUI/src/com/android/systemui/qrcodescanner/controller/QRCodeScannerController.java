19b778a514f17309ef30f819d722cc1cfbe24908
maxwen <max.weninger@gmail.com>
2022-12-14 22:57:20 +0100

    SystemUI: write initial value of SHOW_QR_CODE_SCANNER_SETTING on first call
    and reset only in unregisterDefaultQRCodeScannerObserver
    so it is in sync with isActivityCallable
    
    Change-Id: If4fc2c541d0cee90b49b5769d34adc9d7bffbb02
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qrcodescanner/controller/QRCodeScannerController.java
+++ packages/SystemUI/src/com/android/systemui/qrcodescanner/controller/QRCodeScannerController.java
@@ -361,8 +361,6 @@ public class QRCodeScannerController implements
 
         // Reset cached values to default as we are no longer listening
         mQRCodeScannerPreferenceObserver = new HashMap<>();
-        mSecureSettings.putStringForUser(Settings.Secure.SHOW_QR_CODE_SCANNER_SETTING, null,
-                mUserTracker.getUserId());
     }
 
     private void unregisterDefaultQRCodeScannerObserver() {
@@ -371,6 +369,8 @@ public class QRCodeScannerController implements
 
         // Reset cached values to default as we are no longer listening
         mOnDefaultQRCodeScannerChangedListener = null;
+        mSecureSettings.putStringForUser(Settings.Secure.SHOW_QR_CODE_SCANNER_SETTING, null,
+                mUserTracker.getUserId());
     }
 
     private void notifyQRCodeScannerActivityChanged() {
@@ -398,7 +398,10 @@ public class QRCodeScannerController implements
 
         // While registering the observers for the first time update the default values in the
         // background
-        mExecutor.execute(() -> updateQRCodeScannerActivityDetails());
+        mExecutor.execute(() -> {
+                updateQRCodeScannerActivityDetails();
+                updateQRCodeScannerPreferenceDetails(/* updateSettings = */true);
+                });
         mOnDefaultQRCodeScannerChangedListener =
                 properties -> {
                     if (DeviceConfig.NAMESPACE_SYSTEMUI.equals(properties.getNamespace())
