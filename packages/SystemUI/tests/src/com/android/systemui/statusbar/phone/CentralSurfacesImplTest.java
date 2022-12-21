7f669e51b0c25540acd1aeee0c29637f3208b094
Christian Oder <myself5@carbonrom.org>
2022-12-22 00:14:09 +0100

    fwb: Restore mMainHandler in CentralSurfacesImpl
    This has been removed in c094cbe6bbe49bc0629212119c0c4e56db8a70bf for being unecessary.
    However, we require this for some of our features.
    
    Change-Id: I7e4691f0cd3a569535b541d89baeb7e662004668
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
@@ -625,6 +625,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 mLockscreenTransitionController,
                 mFeatureFlags,
                 mKeyguardUnlockAnimationController,
+                new Handler(TestableLooper.get(this).getLooper()),
                 mMainExecutor,
                 mMessageRouter,
                 mWallpaperManager,
