7a2cfef92a5a12c03f0684ef07d3a1f5b88c1057
Pranav Vashi <neobuddy89@gmail.com>
2023-02-26 16:57:25 +0530

    AppLock: Allow locking all apps visible in launcher
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/evolution/Utils.java
+++ core/java/com/android/internal/util/evolution/Utils.java
@@ -27,6 +27,7 @@ import android.content.om.OverlayInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.AsyncTask;
 import android.os.PowerManager;
 import android.os.SystemClock;
@@ -34,6 +35,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
@@ -98,6 +100,24 @@ public class Utils {
         } catch (PackageManager.NameNotFoundException notFound) {
             return false;
         }
+    }
+
+    public static List<String> launchablePackages(Context context) {
+        List<String> list = new ArrayList<>();
+
+        Intent filter = new Intent(Intent.ACTION_MAIN, null);
+        filter.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> apps = context.getPackageManager().queryIntentActivities(filter,
+                PackageManager.GET_META_DATA);
+
+        int numPackages = apps.size();
+        for (int i = 0; i < numPackages; i++) {
+            ResolveInfo app = apps.get(i);
+            list.add(app.activityInfo.packageName);
+        }
+
+        return list;
     }
 
     public static void switchScreenOff(Context ctx) {
