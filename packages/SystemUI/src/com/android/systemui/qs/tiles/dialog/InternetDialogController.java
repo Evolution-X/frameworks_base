cd6faa56a191702760c63cc299dd76dbd098b40e
Pranav Vashi <neobuddy89@gmail.com>
2023-04-14 16:16:52 +0530

    SystemUI: Prevent InternetDialog crashing system
    This is more of band-aid solution.
    
    Log:
    04-14 05:56:39.878 D/AndroidRuntime(2251): Shutting down VM
    04-14 05:56:39.879 E/AndroidRuntime(2251): FATAL EXCEPTION: main
    04-14 05:56:39.879 E/AndroidRuntime(2251): Process: com.android.systemui, PID: 2251
    04-14 05:56:39.879 E/AndroidRuntime(2251): android.net.ConnectivityManager$TooManyRequestsException
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.net.ConnectivityManager.convertServiceException(ConnectivityManager.java:4008)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.net.ConnectivityManager.sendRequestForNetwork(ConnectivityManager.java:4197)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.net.ConnectivityManager.registerDefaultNetworkCallbackForUid(ConnectivityManager.java:4726)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.net.ConnectivityManager.registerDefaultNetworkCallback(ConnectivityManager.java:4693)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.net.ConnectivityManager.registerDefaultNetworkCallback(ConnectivityManager.java:4667)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.systemui.qs.tiles.dialog.InternetDialog.onStart(InternetDialog.java:171)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.app.Dialog.show(Dialog.java:333)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.systemui.animation.DialogLaunchAnimator.show(DialogLaunchAnimator.kt:600)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.systemui.animation.DialogLaunchAnimator.showFromView(DialogLaunchAnimator.kt:56)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.systemui.qs.tiles.dialog.InternetDialogFactory.create(InternetDialogFactory.kt:55)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.systemui.qs.tiles.InternetTile$$ExternalSyntheticLambda0.run(R8$$SyntheticClass:18)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.os.Handler.handleCallback(Handler.java:942)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.os.Handler.dispatchMessage(Handler.java:99)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.os.Looper.loopOnce(Looper.java:201)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.os.Looper.loop(Looper.java:288)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at android.app.ActivityThread.main(ActivityThread.java:7896)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at java.lang.reflect.Method.invoke(Native Method)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    04-14 05:56:39.879 E/AndroidRuntime(2251): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:854)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
@@ -317,7 +317,11 @@ public class InternetDialogController implements AccessPointController.AccessPoi
         mSubIdTelephonyManagerMap.put(mDefaultDataSubId, mTelephonyManager);
         registerInternetTelephonyCallback(mTelephonyManager, mDefaultDataSubId);
         // Listen the connectivity changes
-        mConnectivityManager.registerDefaultNetworkCallback(mConnectivityManagerNetworkCallback);
+        try {
+            mConnectivityManager.registerDefaultNetworkCallback(mConnectivityManagerNetworkCallback);
+        } catch (Exception e) {
+            // Do nothing
+        }
         mCanConfigWifi = canConfigWifi;
         scanWifiAccessPoints();
     }
