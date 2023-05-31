fecb43c1bf6683f441c8f9009bda291358b34ec2
someone5678 <nemui3353@gmail.com>
2023-05-31 22:33:20 +0900

    [SQUASH] base: Hide ADB and developer setting enable status [1/2]
    * Some apps check whether ADB or developer setting is enabled
    * Return 0 to selected apps when those apps getInt
      ADB_ENABLED, ADB_WIFI_ENABLED, DEVELOPMENT_SETTINGS_ENABLED
    * Code is adapted and stripped down from cutout force full screen
    
    Ref:
    https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    https://github.com/yaap/frameworks_base/commit/232260f192effc9fb21a54eb444601179b24a26d
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    base: HideDevStatus: move to getIntForUser to cover more scenarios
    
    * Adapt to current project as we want to set status individually per-app
      for case apps require ADB or Dev settings for their functionality
    
    e.g. SystemUI Tuner by Zachary Wander
    https://play.google.com/store/apps/details?id=com.zacharee1.systemuituner&hl=en_US&gl=US
    
    Ref:
    https://github.com/RisingTechOSS/android_frameworks_base/commit/77be7be42060dc5119c6dccef874c9a439aed210
    
    Change-Id: Ibbf92c7b1f1181d2508cb22ff2fc2017f9e59706
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    base: HideDeveloperStatus: Query all apps installed by any user [1/2]
    
    * And set value for all user
    
    Change-Id: Ibd2a5dfab832e6bdc6d074ce41401f4905602152
    
    base: HideDeveloperStatusUtils: Avoid NPE with mApps
    
    * https://paste.evolution-x.org/P9Lj8g.
    
    * Avoid NPE

---
+++ core/java/com/android/internal/util/evolution/HideDeveloperStatusUtils.java
@@ -0,0 +1,81 @@
+package com.android.internal.util.evolution;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import android.provider.Settings;
+
+public class HideDeveloperStatusUtils {
+    private static Set<String> mApps = new HashSet<>();
+    private static final Set<String> settingsToHide = new HashSet<>(Arrays.asList(
+        Settings.Global.ADB_ENABLED,
+        Settings.Global.ADB_WIFI_ENABLED,
+        Settings.Global.DEVELOPMENT_SETTINGS_ENABLED
+    ));
+
+    public static boolean shouldHideDevStatus(ContentResolver cr, String packageName, String name) {
+        if (cr == null || packageName == null) {
+            return false;
+        }
+
+        Set<String> apps = getApps(cr);
+        if (apps == null || apps.isEmpty()) {
+            return false;
+        }
+
+        return apps.contains(packageName) && settingsToHide.contains(name);
+    }
+
+    private static Set<String> getApps(ContentResolver cr) {
+        if (cr == null) {
+            return new HashSet<>();
+        }
+
+        String apps = Settings.Secure.getString(cr, Settings.Secure.HIDE_DEVELOPER_STATUS);
+        if (apps != null) {
+            mApps = new HashSet<>(Arrays.asList(apps.split(",")));
+        } else {
+            mApps = new HashSet<>();
+        }
+        return mApps;
+    }
+
+    public void addApp(Context mContext, String packageName, int userId) {
+        if (mContext == null || packageName == null) {
+            return;
+        }
+
+        mApps.add(packageName);
+        Settings.Secure.putStringForUser(mContext.getContentResolver(),
+                Settings.Secure.HIDE_DEVELOPER_STATUS, String.join(",", mApps), userId);
+    }
+
+    public void removeApp(Context mContext, String packageName, int userId) {
+        if (mContext == null || packageName == null) {
+            return;
+        }
+
+        mApps.remove(packageName);
+        Settings.Secure.putStringForUser(mContext.getContentResolver(),
+                Settings.Secure.HIDE_DEVELOPER_STATUS, String.join(",", mApps), userId);
+    }
+
+    public void setApps(Context mContext, int userId) {
+        if (mContext == null) {
+            return;
+        }
+
+        String apps = Settings.Secure.getStringForUser(mContext.getContentResolver(),
+                Settings.Secure.HIDE_DEVELOPER_STATUS, userId);
+        if (apps != null) {
+            mApps = new HashSet<>(Arrays.asList(apps.split(",")));
+        } else {
+            mApps = new HashSet<>();
+        }
+    }
+}
