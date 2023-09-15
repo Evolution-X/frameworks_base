3a6c990bae8c62aa0c578a67d313df33d860d143
elpaablo <donopwhile1@gmail.com>
2023-09-15 03:19:33 +0100

    Smart5gService: guard it with overlay

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1825,9 +1825,11 @@ public final class SystemServer implements Dumpable {
                 t.traceEnd();
             }
 
-            t.traceBegin("StartSmart5gService");
-            mSystemServiceManager.startService(Smart5gService.class);
-            t.traceEnd();
+            if (context.getResources().getBoolean(R.bool.config_supportSmart5G)) {
+                t.traceBegin("StartSmart5gService");
+                mSystemServiceManager.startService(Smart5gService.class);
+                t.traceEnd();
+            }
 
             if (AppFunctionManagerConfiguration.isSupported(context)) {
                 t.traceBegin("StartAppFunctionManager");
