8ebcde2070939a68b9e69b8f34743d8e90fe89d5
Ido Ben-Hur <idoybh2@gmail.com>
2023-12-21 16:02:57 +0200

    base: Allow disabling private DNS for VPN [1/2]
    Allows using the VPN's DNS instead of the set private DNS
    Automatically restores the previous set mode

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -259,5 +259,6 @@ public class SecureSettings {
         Settings.Secure.EVEN_DIMMER_ACTIVATED,
         Settings.Secure.EVEN_DIMMER_MIN_NITS,
         Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE,
+        Settings.Secure.VPN_ENFORCE_DNS,
     };
 }
