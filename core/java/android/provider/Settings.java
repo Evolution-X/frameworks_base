8f54b47bf7208e4e78411136426ccc51a67120b4
someone5678 <someone5678@users.noreply.github.com>
2024-02-09 03:41:40 +0900

    base: Spoof current storage encryption status [1/2]
    * Some apps like Health Connect wants device to be encrypted
    * Spoof cyrpto state to encrypted while disabling encryption possible
    
    Change-Id: Ied3e080f3c91da861ea955692b991e6aee0946d4
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -19779,6 +19779,13 @@ public final class Settings {
         public static final String NO_STORAGE_RESTRICT = "no_storage_restrict";
 
         /**
+         * Control whether to spoof current status of storage encryption.
+         * @hide
+         */
+        @Readable
+        public static final String SPOOF_STORAGE_ENCRYPTION_STATUS = "spoof_storage_encryption_status";
+
+        /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
          *
          * Values are:
