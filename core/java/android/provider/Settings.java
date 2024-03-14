b87987c856e6c073b6807e22d4e9316963f8bd40
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 18:30:27 +0800

    SystemUI: Implement intensity-based QS tiles haptic feedback
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7244,6 +7244,11 @@ public final class Settings {
         public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
 
         /**
+         * @hide
+         */
+        public static final String QS_PANEL_TILE_HAPTIC = "qs_panel_tile_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
