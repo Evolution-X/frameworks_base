05888ee894c6e8388496cab585a01c031935aeff
Tim Yu <yunicorn@google.com>
2024-04-03 18:18:05 +0000

    [RESTRICT AUTOMERGE] Fix Autofill Inflating as User 0
    Autofill will inflate views as the current foreground user instead of
    the system_server user (0).
    
    Bug: 327137311
    Test: atest CtsAutoFillServiceTestCases --user-type secondary_user
    Flag: EXEMPT security bug fix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:2324e00311c0b3326be4389f2905fed270298864
    Merged-In: I8a2c96259976e61d297dec914d10d86e585a6d6f
    Change-Id: I8a2c96259976e61d297dec914d10d86e585a6d6f

--- services/autofill/java/com/android/server/autofill/ui/FillUi.java
+++ services/autofill/java/com/android/server/autofill/ui/FillUi.java
@@ -106,6 +106,7 @@ final class FillUi {
             new AutofillWindowPresenter();
 
     private final @NonNull Context mContext;
+    private final @NonNull Context mUserContext;
 
     private final @NonNull AnchoredWindow mWindow;
 
@@ -140,6 +141,8 @@ final class FillUi {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LEANBACK);
     }
 
+    // System has all permissions, see b/228957088
+    @SuppressWarnings("AndroidFrameworkRequiresPermission")
     FillUi(@NonNull Context context, @NonNull FillResponse response,
             @NonNull AutofillId focusedViewId, @Nullable String filterText,
             @NonNull OverlayControl overlayControl, @NonNull CharSequence serviceLabel,
@@ -153,6 +156,7 @@ final class FillUi {
         mFullScreen = isFullScreen(context);
         mContext = new ContextThemeWrapper(context, mThemeId);
         mMaxInputLengthForAutofill = maxInputLengthForAutofill;
+        mUserContext = Helper.getUserContext(mContext);
 
         final LayoutInflater inflater = LayoutInflater.from(mContext);
 
@@ -244,7 +248,7 @@ final class FillUi {
                     throw new RuntimeException("Permission error accessing RemoteView");
                 }
                 content = response.getPresentation().applyWithTheme(
-                        mContext, decor, interceptionHandler, mThemeId);
+                        mUserContext, decor, interceptionHandler, mThemeId);
                 container.addView(content);
             } catch (RuntimeException e) {
                 callback.onCanceled();
@@ -285,7 +289,7 @@ final class FillUi {
             if (headerPresentation != null) {
                 interactionBlocker = newInteractionBlocker();
                 mHeader = headerPresentation.applyWithTheme(
-                        mContext, null, interactionBlocker, mThemeId);
+                        mUserContext, null, interactionBlocker, mThemeId);
                 final LinearLayout headerContainer =
                         decor.findViewById(R.id.autofill_dataset_header);
                 applyCancelAction(mHeader, response.getCancelIds());
@@ -304,7 +308,7 @@ final class FillUi {
                         interactionBlocker = newInteractionBlocker();
                     }
                     mFooter = footerPresentation.applyWithTheme(
-                            mContext, null, interactionBlocker, mThemeId);
+                            mUserContext, null, interactionBlocker, mThemeId);
                     applyCancelAction(mFooter, response.getCancelIds());
                     // Footer not supported on some platform e.g. TV
                     if (sVerbose) Slog.v(TAG, "adding footer");
@@ -333,7 +337,7 @@ final class FillUi {
                     try {
                         if (sVerbose) Slog.v(TAG, "setting remote view for " + focusedViewId);
                         view = presentation.applyWithTheme(
-                                mContext, null, interceptionHandler, mThemeId);
+                                mUserContext, null, interceptionHandler, mThemeId);
                     } catch (RuntimeException e) {
                         Slog.e(TAG, "Error inflating remote views", e);
                         continue;
@@ -802,6 +806,7 @@ final class FillUi {
         pw.print(prefix); pw.print("mContentHeight: "); pw.println(mContentHeight);
         pw.print(prefix); pw.print("mDestroyed: "); pw.println(mDestroyed);
         pw.print(prefix); pw.print("mContext: "); pw.println(mContext);
+        pw.print(prefix); pw.print("mUserContext: "); pw.println(mUserContext);
         pw.print(prefix); pw.print("theme id: "); pw.print(mThemeId);
         switch (mThemeId) {
             case THEME_ID_DARK:
