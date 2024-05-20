70c83dcc7185f54b48f5868275e15797c307cade
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-20 13:00:37 +0800

    services: Introduce Three finger swipe gestures [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1129,7 +1129,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.THREE_FINGER_GESTURE), false, this,
+                    "three_finger_gesture_action"), false, this,
                     UserHandle.USER_ALL);
 
             updateSettings();
@@ -3454,7 +3454,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             boolean threeFingerGesture = Settings.System.getIntForUser(resolver,
-                    Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
+                    "three_finger_gesture_action", 0, UserHandle.USER_CURRENT) != 0;
             if (mSwipeToScreenshot != null) {
                 if (haveEnableGesture != threeFingerGesture) {
                     haveEnableGesture = threeFingerGesture;
@@ -6945,12 +6945,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mVrManagerInternal.addPersistentVrModeStateListener(mPersistentVrModeListener);
         }
 
-        mSwipeToScreenshot = new SwipeToScreenshotListener(mContext, new SwipeToScreenshotListener.Callbacks() {
-            @Override
-            public void onSwipeThreeFinger() {
-                interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
-            }
-        });
+        mSwipeToScreenshot = new SwipeToScreenshotListener(mContext, (SwipeToScreenshotListener.Callbacks) gestureCallbacks);
 
         mLineageHardware = LineageHardwareManager.getInstance(mContext);
         // Ensure observe happens in systemReady() since we need
