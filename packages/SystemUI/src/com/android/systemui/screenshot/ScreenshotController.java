55c266918e3a15e21f142a1f8903f31116e06bca
someone5678 <someone5678@users.noreply.github.com>
2024-06-06 19:20:29 +0900

    SystemUI: screenshot: Catch RejectedExecutionException
    Log:
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: FATAL EXCEPTION: main
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: Process: com.android.systemui:screenshot, PID: 28408
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: java.util.concurrent.RejectedExecutionException: Task com.android.systemui.screenshot.ScreenshotController$2$$ExternalSyntheticLambda0@ade1538 rejected from java.util.concurrent.ThreadPoolExecutor@c564911[Terminated, pool size = 0, active threads = 0, queued tasks = 0, completed tasks = 1]
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2082)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at java.util.concurrent.ThreadPoolExecutor.reject(ThreadPoolExecutor.java:842)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at java.util.concurrent.ThreadPoolExecutor.execute(ThreadPoolExecutor.java:1374)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at java.util.concurrent.Executors$DelegatedExecutorService.execute(Executors.java:678)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at com.android.systemui.screenshot.ScreenshotController$2.onTaskStackChanged(ScreenshotController.java:357)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at com.android.systemui.shared.system.TaskStackChangeListeners$Impl.handleMessage(TaskStackChangeListeners.java:339)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:103)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:232)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:317)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8502)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:555)
    06-06 17:43:28.612 28408 28408 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:878)
    
    Change-Id: Id96d551134c36837cbe14dc46ab27bee620942ab

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -124,6 +124,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.function.Consumer;
 
 import javax.inject.Provider;
@@ -351,7 +352,10 @@ public class ScreenshotController {
     private final TaskStackChangeListener mTaskListener = new TaskStackChangeListener() {
         @Override
         public void onTaskStackChanged() {
-            mBgExecutor.execute(() -> updateForegroundTaskSync());
+            try {
+                mBgExecutor.execute(() -> updateForegroundTaskSync());
+            } catch (RejectedExecutionException e) {
+            }
         }
     };
 
