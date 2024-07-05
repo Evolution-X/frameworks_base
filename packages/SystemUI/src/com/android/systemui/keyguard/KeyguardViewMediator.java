369a5dc85d25248be67b4d67ac3f344dc00fd66e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-05 09:03:05 +0800

    Disable checks to allow explicit GC for sysui/system_server
    test: lock/unlock device
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -82,6 +82,7 @@ import android.os.Message;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.StrictMode;
 import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
@@ -2934,9 +2935,12 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             userActivity();
             mUpdateMonitor.setKeyguardGoingAway(false);
             mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false);
+            final int oldMask = StrictMode.getThreadPolicyMask();
+            StrictMode.setThreadPolicyMask(0);
             System.gc();
             System.runFinalization();
             System.gc();
+            StrictMode.setThreadPolicyMask(oldMask);
             if (mShowKeyguardWakeLock != null)
                 mShowKeyguardWakeLock.release();
         }
