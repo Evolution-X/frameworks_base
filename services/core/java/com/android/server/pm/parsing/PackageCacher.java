6d50349e34ba5ff6b36edebed3985f627fae4387
Dmitrii <bankersenator@gmail.com>
2024-07-07 16:38:40 +0000

    fwb: fix packagecacher spam
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/parsing/PackageCacher.java
+++ services/core/java/com/android/server/pm/parsing/PackageCacher.java
@@ -272,7 +272,7 @@ public class PackageCacher implements IPackageCacher {
             try (FileOutputStream fos = new FileOutputStream(cacheFile)) {
                 fos.write(cacheEntry);
             } catch (IOException ioe) {
-                Slog.w(TAG, "Error writing cache entry.", ioe);
+                //Slog.w(TAG, "Error writing cache entry.", ioe);
                 cacheFile.delete();
             }
         } catch (Throwable e) {
