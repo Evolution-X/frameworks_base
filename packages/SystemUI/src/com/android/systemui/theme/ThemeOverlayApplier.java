1ee415ca96c9a745ca4fbe7b22512de1c95b994d
elpaablo <donopwhile1@gmail.com>
2024-07-17 07:32:05 +0100

    Implement QS Split Shade (1/3)
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -131,11 +131,20 @@ public class ThemeOverlayApplier implements Dumpable {
     static final String OVERLAY_CATEGORY_NAVBAR =
             "android.theme.customization.navbar";
 
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT =
+            "android.theme.customization.qs_landscape_layout";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT =
+            "android.theme.customization.qs_landscape_cutout";
+
     /*
      * All theme customization categories used by the system, in order that they should be applied,
      * starts with launcher and grouped by target package.
      */
     static final List<String> THEME_CATEGORIES = Lists.newArrayList(
+            OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT,
+            OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT,
             OVERLAY_CATEGORY_SYSTEM_PALETTE,
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_FONT,
@@ -152,6 +161,8 @@ public class ThemeOverlayApplier implements Dumpable {
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
     static final Set<String> SYSTEM_USER_CATEGORIES = Sets.newHashSet(
+            OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT,
+            OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT,
             OVERLAY_CATEGORY_SYSTEM_PALETTE,
             OVERLAY_CATEGORY_ACCENT_COLOR,
             OVERLAY_CATEGORY_DYNAMIC_COLOR,
@@ -193,6 +204,8 @@ public class ThemeOverlayApplier implements Dumpable {
                 Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI));
         mTargetPackageToCategories.put(SETTINGS_PACKAGE,
                 Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS));
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_LANDSCAPE_LAYOUT, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_QS_LANDSCAPE_CUTOUT, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ACCENT_COLOR, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_DYNAMIC_COLOR, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_FONT, ANDROID_PACKAGE);
