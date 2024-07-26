1063099d95c4c5735f785de6ad61cfa78ffd19db
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-26 22:37:35 +0800

    Pocket Mode: Implement doze and biometrics hooks
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6306,7 +6306,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mKeyguardDelegate.onStartedGoingToSleep(pmSleepReason);
         }
         if (mPocketMode != null) {
-            mPocketMode.setDozeState(isDozeMode());
             mPocketMode.onInteractiveChanged(false);
         }
     }
@@ -6383,7 +6382,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mCameraGestureTriggered = false;
 
         if (mPocketMode != null) {
-            mPocketMode.setDozeState(isDozeMode());
             mPocketMode.onInteractiveChanged(true);
         }
     }
