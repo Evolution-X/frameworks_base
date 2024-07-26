08b6902913e7545410b0e3b3ccb31b1310191a57
someone5678 <someone5678@users.noreply.github.com>
2024-07-26 22:37:34 +0900

    Extends "base: Implement cutout force full screen [1/2]"
    * We don't need to guard entire setting by checking cutout
      as this feature is basically LineageOS' long screen setting with cutout force full screen support
    
    * Guard cutout force full screen logic with CutoutUtils' hasCutout check
      in PhoneWindow
    
    * Remove isAvailable check as we enabled it by default
    
    Change-Id: I3c0b6e32307ccc0b12c6247218d31c8e1b7d3623

--- core/java/com/android/internal/policy/PhoneWindow.java
+++ core/java/com/android/internal/policy/PhoneWindow.java
@@ -136,6 +136,8 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.android.internal.util.evolution.cutout.CutoutUtils;
+
 /**
  * Android-specific Window.
  * <p>
@@ -2642,7 +2644,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
             params.layoutInDisplayCutoutMode = mode;
         }
 
-        if (ActivityManager.isSystemReady()) {
+        if (CutoutUtils.hasCutout(context) && ActivityManager.isSystemReady()) {
             try {
                 String packageName = context.getBasePackageName();
                 if (ActivityManager.getService().shouldForceCutoutFullscreen(packageName)){
