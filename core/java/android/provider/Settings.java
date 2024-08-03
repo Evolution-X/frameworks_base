1913f4838968a7d50f1e0bd51e5bec43e12b7f52
AnkitGourav <Ankitgourav035@gmail.com>
2024-08-03 15:03:58 +0530

    SystemUI: Battery bar charging animation color [1/2]
    Change-Id: I909446f588f6abd32b3cfd7c7ccd29695ceb0660
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7103,6 +7103,12 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_USE_GRADIENT_COLOR = "statusbar_battery_bar_use_gradient_color";
 
         /**
+         * Battery bar charging animation color
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE_COLOR = "statusbar_battery_bar_animate_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
