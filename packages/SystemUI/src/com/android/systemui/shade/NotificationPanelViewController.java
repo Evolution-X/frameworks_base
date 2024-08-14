d2f43ea4d1071d60b27b05378aced10dc6f1e85e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-14 08:49:33 +0800

    SystemUI: Show island notification on landscape
    * HUN seems broken on splitshade landscape
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4710,11 +4710,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                     break;
                 case ISLAND_NOTIFICATION:
                     mUseIslandNotification = TunerService.parseIntegerSwitch(newValue, false);
-                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
                     break;
                 case HEADS_UP_NOTIFICATIONS_ENABLED:
                     mUseHeadsUp = TunerService.parseIntegerSwitch(newValue, true);
-                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
                     break;
                 default:
                     break;
@@ -5402,15 +5400,20 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     @Override
     public void showIsland(boolean show) {
-        if (mUseIslandNotification && mUseHeadsUp) {
+        if (useIslandNotification() && mUseHeadsUp) {
             mNotifIsland.showIsland(show, getExpandedFraction());
         }
     }
 
     protected void updateIslandVisibility() {
-        if (mUseIslandNotification && mUseHeadsUp) {
+        if (useIslandNotification() && mUseHeadsUp) {
             mNotifIsland.updateIslandVisibility(getExpandedFraction());
         }
+    }
+    
+    private boolean useIslandNotification() {
+        return mUseIslandNotification || mView.getContext().getResources().getConfiguration().orientation 
+            == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     public void updateMaxDisplayedNotificationsWrapper(boolean recompute) {
