9e48ef6c0e49930932a5d83318a36893e66e4214
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    Initial implementation of KeyboxImitationHooks
    Co-authored-by: chiteroman <querveros999@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -495,6 +495,13 @@ public final class PixelPropsUtils {
     public static void onEngineGetCertificateChain() {
         if (!SystemProperties.getBoolean(SPOOF_PI, true))
             return;
+
+        // If a keybox is found, don't block key attestation
+        if (KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
+            return;
+        }
+
         // Check stack for SafetyNet or Play Integrity
         if (isCallerSafetyNet() && !sIsExcluded) {
             dlog("Blocked key attestation");
