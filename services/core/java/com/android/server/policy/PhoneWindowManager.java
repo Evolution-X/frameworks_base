bdc15582bb24de684c566dae48e976144f243ade
blackshibe <kacperszczepan@tutanota.com>
2024-10-06 01:43:06 +0200

    PhoneWindowManager: Add vibration when using physical camera button gesture
    Change-Id: Ie6be66cf986869c93e1cf58738a1b2fc740d73de

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5253,6 +5253,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         }
                         if (!isAlreadyRunning(intent)) {
                             isWakeKey = true;
+                            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, "Camera key - Launch Camera");
                             startActivityAsUser(intent, UserHandle.CURRENT_OR_SELF);
                         }
                     }
