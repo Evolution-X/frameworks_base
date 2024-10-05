ec705c1e7c0ee0fe54f78c0a35b113796d9ba4ae
Pranav Vashi <neobuddy89@gmail.com>
2024-10-06 01:24:30 +0530

    SystemUI: Allow to toggle BT directly with Bluetooth tile [1/2]
    * We do not want to lose default behavior that matches with the internet tile
      The default behavior is to show dialog.
    * However, disabling dialog - will match the behavior with the wifi tile.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.HandlerExecutor;
 import android.os.Looper;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
@@ -116,7 +117,9 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
             return;
         }
 
-        if (mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG)) {
+        boolean showDialog = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_BT_SHOW_DIALOG, 1, UserHandle.USER_CURRENT) != 0;
+        if (showDialog && mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG)) {
             mDialogViewModel.showDialog(mContext, view);
         } else {
             // Secondary clicks are header clicks, just toggle.
@@ -204,7 +207,9 @@ public class BluetoothTile extends SecureQSTile<BooleanState> {
         }
 
         state.expandedAccessibilityClassName = Switch.class.getName();
-        state.forceExpandIcon = mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG);
+        boolean showDialog = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_BT_SHOW_DIALOG, 1, UserHandle.USER_CURRENT) != 0;
+        state.forceExpandIcon = showDialog && mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG);
     }
 
     /**
