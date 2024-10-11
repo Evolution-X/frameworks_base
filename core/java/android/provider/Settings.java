1be0d080dfe292345693111ea300234c11aeb913
blackshibe <kacperszczepan@tutanota.com>
2024-10-11 17:48:46 +0200

    SystemUI: Add ability to hide carrier name on lockscreen [1/2]
    [someone5678]
    * Minor clean-ups and edit
    * Make it default to on (Show carrier text)
    
    [ShevT]
    * Adapt for A15
    
    [neobuddy89]
    * Code clean up!
    
    Original commit:
    https://github.com/blackshibe/android_frameworks_base/commit/080225e4e911c62315f1e5cf6e0488705d90f0ed
    
    Change-Id: I5951c88d789cf66720b702d61e156982526dcf59
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6954,6 +6954,12 @@ public final class Settings {
         public static final String BLOCK_WALLPAPER_DIMMING = "block_wallpaper_dimming";
 
         /**
+         * Whether to show the carrier name on the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHOW_CARRIER = "lockscreen_show_carrier";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
