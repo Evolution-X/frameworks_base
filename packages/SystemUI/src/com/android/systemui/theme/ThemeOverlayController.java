4a44647f10acd9f5b50a89b502855735060eab00
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-14 15:12:06 +0800

    SystemUI: Introduce ThemeController
    Change-Id: Ib2e957d115b1e41feb6136d7fa0a23dcf3fc8a5d
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -197,6 +197,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     private final UserScopedService<UiModeManager> mUiModeManagerProvider;
     private ColorScheme mDarkColorScheme;
     private ColorScheme mLightColorScheme;
+    private final RisingThemeController mThemeController;
 
     // Defers changing themes until Setup Wizard is done.
     private boolean mDeferredThemeEvaluation;
@@ -501,6 +502,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 .isFinishedInStateWhereWithScene(KeyguardState.Companion::deviceIsAsleepInState);
 
         mIsKeyguardOnAsleepState = mJavaAdapter.stateInApp(isFinishedInAsleepStateFlow, false);
+        mThemeController = new RisingThemeController(mContext, mBgHandler);
     }
 
     @Override
@@ -527,6 +529,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         final IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_PROFILE_ADDED);
         filter.addAction(Intent.ACTION_WALLPAPER_CHANGED);
+        mThemeController.observeSettings(() -> reevaluateSystemTheme(true));
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, mMainExecutor,
                 UserHandle.ALL);
         mSecureSettings.registerContentObserverForUserSync(
