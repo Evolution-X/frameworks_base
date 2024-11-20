92f7a2cf88f5cc63328c58c34719b4bef874979f
Ghosuto <clash.raja10@gmail.com>
2024-11-20 10:01:29 +0000

    SystemUI: Simple analog clock
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -57,10 +57,12 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
             R.layout.keyguard_clock_nos2,
             R.layout.keyguard_clock_life,
             R.layout.keyguard_clock_word,
-            R.layout.keyguard_clock_encode
+            R.layout.keyguard_clock_encode,
+            R.layout.keyguard_clock_nos3,
+            R.layout.keyguard_clock_analog
     };
 
-    private final static int[] mCenterClocks = {2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16};
+    private final static int[] mCenterClocks = {2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18};
 
     private static final int DEFAULT_STYLE = 0; // Disabled
     public static final String CLOCK_STYLE_KEY = "clock_style";
