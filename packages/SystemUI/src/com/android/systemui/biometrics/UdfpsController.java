ed9fe81ed3a6291432ed6a83d23f3fc2bba19332
John Galt <johngaltfirstrun@gmail.com>
2024-12-11 09:24:05 -0500

    UdfpsController: add LAUNCH boost
    Based on logic from
    https://github.com/AOSPA/android_frameworks_base/commit/94cb933df8fa0cf2eb55c78e72f14b30f690e066
    which used qcom perf framework opposed to cleaner libperfmgr.
    
    Massively improves performance on OP12, should improve performance on
    virtually any device with libperfmgr and a powerhint that has
    significant LAUNCH boosts.

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -55,6 +55,7 @@ import android.os.CancellationSignal;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
+import android.os.PowerManagerInternal;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.VibrationAttributes;
@@ -78,6 +79,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
 import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.server.LocalServices;
 import com.android.systemui.Dumpable;
 import com.android.systemui.animation.ActivityTransitionAnimator;
 import com.android.systemui.biometrics.AuthController;
@@ -236,6 +238,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean mScreenOffFod;
 
     private UdfpsAnimation mUdfpsAnimation;
+
+    PowerManagerInternal mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -1142,6 +1146,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         final View view = mOverlay.getTouchOverlay();
 
+        if (mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerMode(PowerManagerInternal.MODE_LAUNCH, true);
+        }
+
         if (view != null && view.getViewRootImpl() != null) {
             view.getViewRootImpl().notifyRendererOfExpensiveFrame();
         }
@@ -1226,6 +1234,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mOnFingerDown = false;
         unconfigureDisplay(view);
         cancelAodSendFingerUpAction();
+        if (mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerMode(PowerManagerInternal.MODE_LAUNCH, false);
+        }
+
     }
 
     public boolean isAnimationEnabled() {
