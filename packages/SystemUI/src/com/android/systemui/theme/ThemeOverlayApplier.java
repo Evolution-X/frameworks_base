4b37fc471ead9ad1dce8d3696d28d4fb564b632d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-12 13:22:43 +0800

    Themes: Add support for progress bar styles
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -134,6 +134,9 @@ public class ThemeOverlayApplier implements Dumpable {
             "android.theme.customization.lockscreen_clock_font";
     static final String OVERLAY_CATEGORY_NAVBAR =
             "android.theme.customization.navbar";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_PROGRESS_BAR =
+            "android.theme.customization.progress_bar";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -151,7 +154,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SIGNAL,
             OVERLAY_CATEGORY_ICON_WIFI,
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
-            OVERLAY_CATEGORY_NAVBAR);
+            OVERLAY_CATEGORY_NAVBAR,
+            OVERLAY_CATEGORY_PROGRESS_BAR);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -164,7 +168,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_LOCK_CLOCK_FONT,
-            OVERLAY_CATEGORY_NAVBAR);
+            OVERLAY_CATEGORY_NAVBAR,
+            OVERLAY_CATEGORY_PROGRESS_BAR);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -208,6 +213,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_LOCK_CLOCK_FONT, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NAVBAR, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_PROGRESS_BAR, ANDROID_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
