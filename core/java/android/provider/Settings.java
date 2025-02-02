2a2ad1a339ba9ea79be223c5114a6a718db1f1d1
Ido Ben-Hur <idoybh2@gmail.com>
2025-02-02 14:13:43 +0530

    base: Add a toggle for long press on navbar to search [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7249,6 +7249,13 @@ public final class Settings {
         public static final String QS_PANEL_TILE_HAPTIC = "qs_panel_tile_haptic";
 
         /**
+         * Whether long press on navbar gesture is enabled
+         * @hide
+         */
+        @Readable
+        public static final String NAVBAR_LONG_PRESS_GESTURE = "navbar_long_press_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
