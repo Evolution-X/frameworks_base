6f7a166c4bde355e197069e7b41bb910785bbe16
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-02 21:44:31 +0800

    Enable freeform windows by default
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/shared/src/com/android/systemui/shared/system/ActivityManagerWrapper.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/system/ActivityManagerWrapper.java
@@ -302,8 +302,7 @@ public class ActivityManagerWrapper {
      * Returns true if the system supports freeform multi-window.
      */
     public boolean supportsFreeformMultiWindow(Context context) {
-        final boolean freeformDevOption = Settings.Global.getInt(context.getContentResolver(),
-                Settings.Global.DEVELOPMENT_ENABLE_FREEFORM_WINDOWS_SUPPORT, 0) != 0;
+        final boolean freeformDevOption = true;
         return ActivityTaskManager.supportsMultiWindow(context)
                 && (context.getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_FREEFORM_WINDOW_MANAGEMENT)
