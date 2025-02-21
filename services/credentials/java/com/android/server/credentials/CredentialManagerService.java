57d5a566c3d14ff99c6c68589b94b6b2434d9139
Daniel <danieljkim@google.com>
2025-02-21 23:11:36 +0000

    Verify caller is cred autofill service
    Bug: 370477460
    Test: atest CtsAutoFillServiceTestCases:android.autofillservice.cts.inline.InlineLoginMixedCredentialActivityTest
    (cherry picked from commit 571a97ade14d3e008172a1c084bf7331f6fc8875)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e61320b6fcca1b6f0fe60078cc27fcc844a63f12)
    Merged-In: I35f3cf616669a6879fd353876029152e3ce327f1
    Change-Id: I35f3cf616669a6879fd353876029152e3ce327f1

--- services/credentials/java/com/android/server/credentials/CredentialManagerService.java
+++ services/credentials/java/com/android/server/credentials/CredentialManagerService.java
@@ -77,6 +77,7 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -101,6 +102,8 @@ public final class CredentialManagerService
 
     private static final String DEVICE_CONFIG_ENABLE_CREDENTIAL_DESC_API =
             "enable_credential_description_api";
+
+    private static final String CREDMAN_SERVICE_PACKAGE_NAME = "com.android.credentialmanager";
 
     /**
      * Value stored in autofill pref when credential provider is primary. This is
@@ -492,6 +495,17 @@ public final class CredentialManagerService
 
             final int userId = UserHandle.getCallingUserId();
             final int callingUid = Binder.getCallingUid();
+            PackageManager pm = mContext.createContextAsUser(
+                    UserHandle.getUserHandleForUid(callingUid), 0).getPackageManager();
+            String callingProcessPackage = pm.getNameForUid(callingUid);
+            if (callingProcessPackage == null) {
+                throw new SecurityException(
+                        "Couldn't determine the identity of the caller.");
+            }
+            if (!Objects.equals(CREDMAN_SERVICE_PACKAGE_NAME, callingProcessPackage)) {
+                throw new SecurityException(callingProcessPackage
+                        + " is not the device's credential autofill package.");
+            }
 
             // New request session, scoped for this request only.
             final GetCandidateRequestSession session =
