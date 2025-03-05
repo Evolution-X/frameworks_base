ac464790f3726050d186c145227c012e95760a3a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-06 06:16:37 +0800

    PackageManagerService: Wipe resource/package cache on upgrade
    Change-Id: I6fef239ff560abb3bd30acbb8aaee451e9c64feb

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2242,10 +2242,12 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             mIsUpgrade =
                     !partitionsFingerprint.equals(ver.fingerprint);
             if (mIsUpgrade) {
+                SystemProperties.set("persist.sys.is_upgrade", "1");
                 PackageManagerServiceUtils.logCriticalInfo(Log.INFO,
                         "Upgrading from " + ver.fingerprint + " (" + ver.buildFingerprint + ") to "
                                 + PackagePartitions.FINGERPRINT
                                 + " (" + Build.VERSION.INCREMENTAL + ")");
+                SystemProperties.set("persist.sys.is_upgrade", "0");
             }
             mPriorSdkVersion = mIsUpgrade ? ver.sdkVersion : -1;
             mInitAppsHelper = new InitAppsHelper(this, mApexManager, mInstallPackageHelper,
