3fa27ab88f2b2dc868abcd75a50197c4a163c52f
Lixkote <95425619+Lixkote@users.noreply.github.com>
2025-05-06 17:26:28 +0000

    SystemUI: Added android 9 style lockscreen clock
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -59,10 +59,11 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
             R.layout.keyguard_clock_word,
             R.layout.keyguard_clock_encode,
             R.layout.keyguard_clock_nos3,
-            R.layout.keyguard_clock_analog
+            R.layout.keyguard_clock_analog,
+            R.layout.keyguard_clock_a9
     };
 
-    private final static int[] mCenterClocks = {2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18};
+    private final static int[] mCenterClocks = {2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20};
 
     private static final int DEFAULT_STYLE = 0; // Disabled
     public static final String CLOCK_STYLE_KEY = "clock_style";
