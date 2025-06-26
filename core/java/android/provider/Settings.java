f3ce17b9b3bcb0a43f9dd48e2e96870f35008ae6
Joey Huab <jhuab@edukasyon.ph>
2025-06-27 01:03:31 +0900

    Revert "SystemUI: On-The-Go Mode (1/2)"
    This reverts commit c40294619baaef0920afb3494e276f3cbc39988f.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6639,32 +6639,6 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
-         * If On-The-Go should be displayed at the power menu.
-         * @hide
-         */
-        public static final String GLOBAL_ACTIONS_ONTHEGO = "global_actions_onthego";
-
-        /**
-         * The alpha value of the On-The-Go overlay.
-         * @hide
-         */
-        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
-
-        /**
-         * Whether the service should restart itself or not.
-         * @hide
-         */
-        public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
-
-        /**
-         * The camera instance to use.
-         * 0 = Rear Camera
-         * 1 = Front Camera
-         * @hide
-         */
-        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
-
-        /**
          * Whether to display cross sign for a data disabled connection
          * @hide
          */
