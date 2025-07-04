0afd0767b88103ddcd0d4c1b54782ca707fe0dd5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 06:43:05 +0800

    SystemUI: Add lockscreen media art
    Change-Id: I4db66c3e1e98c1ac04a315e8e76ec3799e8f4cbd
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7337,6 +7337,12 @@ public final class Settings {
         public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
 
         /**
+         * Lockscreen Media Art
+         * @hide
+         */
+        public static final String LS_MEDIA_ART_ENABLED = "ls_media_art_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
