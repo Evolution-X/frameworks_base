cdc1e872f70c2e20939753f112bf7bef47d8d42e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-21 21:55:28 +0800

    SystemUI: Add edge light feature
    @neobuddy89:
    * Fix multi-user usage.
    * Define settings key we used earlier.
    
    Change-Id: I2bdf550eadd92d5f914b11ac48101eb8cf368e97
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -29,6 +29,7 @@ import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dagger.qualifiers.PerUser;
 import com.android.systemui.dump.DumpManager;
+import com.android.systemui.edgelight.EdgeLightViewController;
 import com.android.systemui.nowplaying.NowPlayingViewController;
 import com.android.systemui.keyguard.KeyguardSliceProvider;
 import com.android.systemui.media.MediaViewController;
@@ -249,6 +250,9 @@ public interface SysUIComponent {
 
     @SysUISingleton
     PulseViewController pulseViewController();
+
+    @SysUISingleton
+    EdgeLightViewController edgeLightViewController();
 
     @SysUISingleton
     NowPlayingViewController nowPlayingViewController();
