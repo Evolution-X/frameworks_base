0e5a8cd1efa01a999e5a2da65eb90726ebe49989
inthewaves <inthewaves@pm.me>
2025-07-30 13:38:58 -0700

    [temporary] make AOSP dialer call notifications not groupable
    Temporary until Dialer gets CallStyle set for calling notifications. If the Dialer has two or more
    ungrouped notifications, autogrouping will occur. It just so happens that legacy voicemail
    notification is also ungrouped.
    
    Autogrouping call notifications will result in `Notification.FLAG_SILENT` being added to incoming
    call notifications, and that will prevent the fullScreenIntent from showing.

--- services/core/java/com/android/server/notification/GroupHelper.java
+++ services/core/java/com/android/server/notification/GroupHelper.java
@@ -2202,6 +2202,13 @@ public class GroupHelper {
                 return false;
             }
 
+            // temporary until Dialer gets CallStyle set
+            if ("com.android.dialer".equals(record.getSbn().getPackageName())
+                    && Notification.CATEGORY_CALL.equals(
+                            record.getSbn().getNotification().category)) {
+                return false;
+            }
+
             if (record.getSbn().getNotification().isMediaNotification()) {
                 return false;
             }
