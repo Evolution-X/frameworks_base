498fa68b076c49e968adf1760923696a695d3f91
Abhay Singh Gill <abhaygill017@gmail.com>
2025-08-08 21:22:13 +0530

    SystemUI: VolumeDialog: Fix inactive row colors
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -2672,21 +2672,19 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         if (!useActiveColoring && !mChangeVolumeRowTintWhenInactive) {
             return;
         }
-        final ColorStateList colorTint = useActiveColoring
-                ? Utils.getColorAccent(mContext)
-                : Utils.getColorAttr(mContext, com.android.internal.R.attr.colorAccentSecondary);
+        final ColorStateList colorTint = Utils.getColorAccent(mContext);
+
         final int alpha = useActiveColoring
                 ? Color.alpha(colorTint.getDefaultColor())
                 : getAlphaAttr(android.R.attr.secondaryContentAlpha);
 
-        final ColorStateList bgTint = useActiveColoring
-                ? Utils.getColorAttr(mContext, android.R.attr.colorBackgroundFloating)
-                : Utils.getColorAttr(mContext, com.android.internal.R.attr.textColorOnAccent);
+        final ColorStateList bgTint = Utils.getColorAttr(mContext, android.R.attr.colorBackgroundFloating);
 
         final ColorStateList inverseTextTint = Utils.getColorAttr(
                 mContext, com.android.internal.R.attr.textColorOnAccent);
 
         row.sliderProgressSolid.setTintList(colorTint);
+        row.sliderProgressSolid.setAlpha(alpha);
         if (row.sliderProgressIcon != null) {
             row.sliderProgressIcon.setTintList(bgTint);
         }
