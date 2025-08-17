65e61bea98faef3d6f99739358e466935fda0db4
Pranav Vashi <neobuddy89@gmail.com>
2025-08-17 11:25:55 +0530

    SystemUI: Add toggle for QS footer data usage
    * Re-use same key QS_SHOW_DATA_USAGE for new implementation.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7088,6 +7088,12 @@ public final class Settings {
         public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
 
         /**
+         * Whether to show daily data usage in the QS footer.
+         * @hide
+         */
+        public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
