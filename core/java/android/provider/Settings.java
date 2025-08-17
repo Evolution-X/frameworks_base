ae17ac1e083ccbdfa07e20bd53f1ed7a529d41ca
Pranav Vashi <neobuddy89@gmail.com>
2025-08-18 02:20:19 +0530

    SystemUI: Add customization to select QS tile shape
    * At present it checks only for the state. Inject setting to pre-select shape.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7104,6 +7104,12 @@ public final class Settings {
         public static final String UDFPS_ICON = "udfps_icon";
 
         /**
+         * Customize QS tile shape.
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
