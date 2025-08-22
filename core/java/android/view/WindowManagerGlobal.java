2349377ebdf8498bb7c8171ad40e01e7a281370c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-22 08:59:03 +0800

    [BUGFIX] fix ignore secure window flags
    Change-Id: Ibae64428fcbb44fb774d04dbe828b179d8a71234
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- core/java/android/view/WindowManagerGlobal.java
+++ core/java/android/view/WindowManagerGlobal.java
@@ -40,6 +40,7 @@ import android.os.Looper;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
+import android.provider.Settings;
 import android.util.AndroidRuntimeException;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -481,6 +482,14 @@ public final class WindowManagerGlobal {
                         windowlessSession, new WindowlessWindowLayout());
             }
 
+            boolean ignoreSecure = Settings.Global.getInt(
+                    view.getContext().getContentResolver(),
+                    Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1;
+
+            if (ignoreSecure) {
+                wparams.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
+            }
+
             view.setLayoutParams(wparams);
 
             mViews.add(view);
@@ -512,6 +521,14 @@ public final class WindowManagerGlobal {
         }
 
         final WindowManager.LayoutParams wparams = (WindowManager.LayoutParams)params;
+
+        boolean ignoreSecure = Settings.Global.getInt(
+                view.getContext().getContentResolver(),
+                Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1;
+
+        if (ignoreSecure) {
+            wparams.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
+        }
 
         view.setLayoutParams(wparams);
 
