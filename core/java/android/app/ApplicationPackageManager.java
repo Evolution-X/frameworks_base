fdfee1b3bd00d5ab6ad850984f6b06480b5ff4aa
Akash Srivastava <akashniki@gmail.com>
2025-09-08 17:56:26 +0200

    PixelPropsUtils: Skip play Integrity props in isolated processes
    * Isolated processes (e.g. com.android.vending:com.google.android.finsky.verifier.apkanalysis.service.ApkContentsScanService) cannot access content providers. Calling Settings.Secure.getString(...) during their application init triggers:
    
    * Log:
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: FATAL EXCEPTION: main
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: Process: com.android.vending:com.google.android.finsky.verifier.apkanalysis.service.ApkContentsScanService, PID: 16766
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: java.lang.RuntimeException: Unable to instantiate application com.google.android.finsky.application.classic.ClassicApplication package com.android.vending: java.lang.SecurityException: Isolated process not allowed to call getContentProvider
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.app.LoadedApk.makeApplicationInner(LoadedApk.java:1486)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.app.LoadedApk.makeApplicationInner(LoadedApk.java:1411)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.app.ActivityThread.handleBindApplication(ActivityThread.java:7827)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.app.ActivityThread.-$$Nest$mhandleBindApplication(Unknown Source:0)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2549)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:110)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:248)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:338)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:9137)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:593)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:940)
    09-06 22:51:42.792 16766 16766 E AndroidRuntime: Caused by: java.lang.SecurityException: Isolated process not allowed to call getContentProvider
    
    * Add Process.isIsolated() guard in setPlayIntegrityProps(Context) and return early. Also gate the call site in setProps(...) to avoid invoking setPlayIntegrityProps(...) from isolated processes, and log the skip.
    
    Change-Id: Iecf42b7ceb2f186128d0f1defbc45bcc4224e56a
    [neobuddy89: Adapted for PixelPropsUtils]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -795,8 +795,8 @@ public class ApplicationPackageManager extends PackageManager {
             final List<FeatureInfo> list = new ArrayList<>(parceledList.getList());
 
             // Inject Tensor features when toggle is enabled
-            final boolean forceTensor = SystemProperties.getBoolean(
-                    "persist.sys.pp.tensor", false);
+            final boolean forceTensor = !Process.isIsolated() &&
+                    SystemProperties.getBoolean("persist.sys.pp.tensor", false);
 
             if (forceTensor && !IS_TENSOR_DEVICE) {
                 for (String feature : FEATURES_TENSOR) {
@@ -948,7 +948,8 @@ public class ApplicationPackageManager extends PackageManager {
         final String pkg = ActivityThread.currentPackageName();
 
         if (name != null && pkg != null && PRIV_PKGS.contains(pkg)) {
-            final boolean photosSpoof = "com.google.android.apps.photos".equals(pkg)
+            final boolean photosSpoof = !Process.isIsolated()
+                && "com.google.android.apps.photos".equals(pkg)
                 && SystemProperties.getBoolean("persist.sys.pp.photos", true);
             if (photosSpoof) {
                 if (FEATURES_PIXEL.contains(name)) return false;
@@ -964,8 +965,8 @@ public class ApplicationPackageManager extends PackageManager {
         }
 
         if (name != null && FEATURES_TENSOR.contains(name)) {
-            final boolean forceTensor = SystemProperties.getBoolean(
-                "persist.sys.pp.tensor", false);
+            final boolean forceTensor = !Process.isIsolated() &&
+                    SystemProperties.getBoolean("persist.sys.pp.tensor", false);
 
             // Do not interfere with real Tensor devices
             if (IS_TENSOR_DEVICE) {
