0d895580f2d6ebdef18d94105180517433b49f3f
Pranav Vashi <neobuddy89@gmail.com>
2025-09-14 13:37:55 +0530

    SystemUI: Add weekly data usage info in QS footer
    * Toggle on single tap to change view.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7088,10 +7088,16 @@ public final class Settings {
         public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
 
         /**
-         * Whether to show daily data usage in the QS footer.
+         * Whether to show daily/weekly data usage in the QS footer.
          * @hide
          */
         public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
+
+        /**
+         * Persist setting for showing either daily or weekly data usage in the QS footer.
+         * @hide
+         */
+        public static final String QS_SHOW_DATA_USAGE_WINDOW = "qs_show_data_usage_window";
 
         /**
          * @hide
