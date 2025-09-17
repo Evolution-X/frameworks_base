ff88f03ef4799b34b0dd2993a587e17f841d5f0d
Joey Huab <jhuab@edukasyon.ph>
2025-09-17 11:13:46 +0900

    SystemUI: Switch custom QS tiles metrics to `QS_PANEL`

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -186,7 +186,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EVOLVER;
+        return MetricsEvent.QS_PANEL;
     }
 
     @Override
