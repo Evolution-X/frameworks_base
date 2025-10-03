f6e1711aead5b120f3ec2ffd66408f9f74c085f8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-03 19:09:21 +0800

    Adding dynamic font feature
    optimized overriding of system fonts without reflection usage and wider font override support
    
    Change-Id: I4940b525584a3ad7c83193ff2484e6726c5437f9
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -261,6 +261,7 @@ import com.android.internal.os.SomeArgs;
 import com.android.internal.os.logging.MetricsLoggerWrapper;
 import com.android.internal.policy.DecorView;
 import com.android.internal.protolog.ProtoLog;
+import com.android.internal.util.android.FontController;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.FastPrintWriter;
 import com.android.internal.util.Preconditions;
@@ -7312,6 +7313,8 @@ public final class ActivityThread extends ClientTransactionHandler
         mConfigurationController.handleConfigurationChanged(config);
         updateDeviceIdForNonUIContexts(deviceId);
 
+        FontController.OnConfigurationChanged(getApplication().getResources());
+
         // These are only done to maintain @UnsupportedAppUsage and should be removed someday.
         mCurDefaultDisplayDpi = mConfigurationController.getCurDefaultDisplayDpi();
         mConfiguration = mConfigurationController.getConfiguration();
@@ -8114,6 +8117,9 @@ public final class ActivityThread extends ClientTransactionHandler
         data.info = getPackageInfo(data.appInfo, mCompatibilityInfo, null /* baseLoader */,
                 false /* securityViolation */, true /* includeCode */,
                 false /* registerPackage */, isSdkSandbox);
+
+        FontController.OnConfigurationChanged(data.info.getResources());
+
         if (isSdkSandbox) {
             data.info.setSdkSandboxStorage(data.sdkSandboxClientAppVolumeUuid,
                     data.sdkSandboxClientAppPackage);
