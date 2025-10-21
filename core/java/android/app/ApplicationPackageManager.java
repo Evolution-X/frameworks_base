c7ffea6c884f25055ca08769f8fba8be9c864691
Pranav Vashi <neobuddy89@gmail.com>
2025-10-21 09:44:15 +0530

    PixelPropsUtils: Move from props to Settings switch
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -795,8 +795,8 @@ public class ApplicationPackageManager extends PackageManager {
             final List<FeatureInfo> list = new ArrayList<>(parceledList.getList());
 
             // Inject Tensor features when toggle is enabled
-            final boolean forceTensor = SystemProperties.getBoolean(
-                    "persist.sys.pp.tensor", false);
+            final boolean forceTensor = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.PI_TENSOR_SPOOF, 0) == 1;
 
             if (forceTensor && !IS_TENSOR_DEVICE) {
                 for (String feature : FEATURES_TENSOR) {
@@ -947,7 +947,8 @@ public class ApplicationPackageManager extends PackageManager {
 
         if (name != null && pkg != null && PRIV_PKGS.contains(pkg)) {
             final boolean photosSpoof = "com.google.android.apps.photos".equals(pkg)
-                && SystemProperties.getBoolean("persist.sys.pp.photos", true);
+                && (Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.PI_PHOTOS_SPOOF, 1) == 1);
             if (photosSpoof) {
                 if (FEATURES_PIXEL.contains(name)) return false;
                 if (FEATURES_PIXEL_OTHERS.contains(name)) return true;
@@ -962,8 +963,8 @@ public class ApplicationPackageManager extends PackageManager {
         }
 
         if (name != null && FEATURES_TENSOR.contains(name)) {
-            final boolean forceTensor = SystemProperties.getBoolean(
-                "persist.sys.pp.tensor", false);
+            final boolean forceTensor = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.PI_TENSOR_SPOOF, 0) == 1;
 
             // Do not interfere with real Tensor devices
             if (IS_TENSOR_DEVICE) {
