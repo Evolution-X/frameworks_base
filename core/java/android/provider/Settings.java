9680fbe41502727bc79b26ad969bc904ac2ec257
Ghosuto <clash.raja10@gmail.com>
2025-10-25 12:56:26 +0000

    SystemUI: Introduce Lock glymps
    Change-Id: Ib696e2ab7b05c1166ab1ed0677423796ab0dc3f3
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7731,6 +7731,33 @@ public final class Settings {
         public static final String QS_TILE_ICON_SHAPE = "qs_tile_icon_shape";
 
         /**
+         * Lock Glymps - Dynamic lock screen wallpaper feature
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_ENABLED = "lock_glymps_enabled";
+
+        /**
+         * Lock Glymps wallpaper source (0=Random, 1=Custom URLs)
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_SOURCE = "lock_glymps_source";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_WIFI_ONLY = "lock_glymps_wifi_only";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_CACHE_SIZE = "lock_glymps_cache_size";
+
+        /**
+         * @hide
+         */
+        public static final String LOCK_GLYMPS_CUSTOM_URLS = "lock_glymps_custom_urls";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
