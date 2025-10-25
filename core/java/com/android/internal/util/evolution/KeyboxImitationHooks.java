f155c600b2096ac549462f92874c055c38bf2c6b
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-26 01:28:33 +0530

    KeyboxImitationHooks: Ignore keys requiring auth
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/com/android/internal/util/evolution/KeyboxImitationHooks.java
+++ core/java/com/android/internal/util/evolution/KeyboxImitationHooks.java
@@ -64,6 +64,10 @@ public class KeyboxImitationHooks {
             return null;
         }
 
+        if (!params.noAuthRequired) {
+            return null;
+        }
+
         if (params.algorithm != Algorithm.EC && params.algorithm != Algorithm.RSA) {
             Log.w(TAG, "Unsupported algorithm: " + params.algorithm);
             return null;
