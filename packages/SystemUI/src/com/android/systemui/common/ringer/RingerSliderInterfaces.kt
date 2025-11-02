add6ea5c255bb0b5ec01ae47a236485d9c9a4150
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-02 23:28:28 +0800

    SystemUI: Introduce ringer qs tile
    Change-Id: Ib8bdda0c45ecef171f3ff4d6b9fff6d8acdc394a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/common/ringer/RingerSliderInterfaces.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2025 AxionOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.common.ringer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+interface RingerSliderTheme {
+    @get:Composable
+    val activeBg: Color
+    
+    @get:Composable
+    val neutralBg: Color
+    
+    @get:Composable
+    val activeIcon: Color
+    
+    @get:Composable
+    val neutralIcon: Color
+    
+    val dozeStroke: Dp
+}
+
+interface RingerSliderDimens {
+    val totalWidth: Dp?
+    val thumbSize: Dp
+    val iconSize: Dp
+    val thumbPadding: Dp
+    val dotSize: Dp
+}
