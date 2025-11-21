55da8e65675de7672a068b163c4518e0ff6025dc
Joey <joey@evolution-x.org>
2025-11-21 21:16:50 +0900

    PixelPropsUtils: Update fingerprints to July 2026 release

--- core/java/com/android/internal/util/evolution/AttestationHooks.java
+++ core/java/com/android/internal/util/evolution/AttestationHooks.java
@@ -52,7 +52,7 @@ public final class AttestationHooks {
         "DEVICE", "mustang",
         "PRODUCT", "mustang",
         "MODEL", "Pixel 10 Pro XL",
-        "FINGERPRINT", "google/mustang/mustang:16/BD3A.251005.003.W3/14147046:user/release-keys"
+        "FINGERPRINT", "google/mustang/mustang:16/CP1A.260305.018/14887507:user/release-keys"
     );
 
     private static final Map<String, Object> sPixel5aProps = Map.of(
