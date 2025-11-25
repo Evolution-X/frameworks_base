1e45c918f4ed6679011f75d5c10eba108221e932
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-25 18:07:42 +0800

    services: Add system server hooks
    Change-Id: Ie2732937474a9daceddea6d6365ea67c7bab9e41
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -4347,6 +4347,10 @@ public final class ProcessList implements ProcessStateController.ProcessLruUpdat
         return mLruProcesses;
     }
 
+    public ArrayList<ProcessRecord> ntGetLruProcesses() {
+        return this.mLruProcesses;
+    }
+
     /**
      * Return the reference to the LRU list, call this function for read/write access
      */
