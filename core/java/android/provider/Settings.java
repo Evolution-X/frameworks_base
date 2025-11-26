629d1a4684309ddf3b7598dd3b61b39a8a79fc4c
Ghosuto <clash.raja10@gmail.com>
2025-11-26 06:50:37 +0000

    SystemUI: Introduce charging effects [1/2]
    Change-Id: I2c66b0f8358157a47f67bf747fac53fe5b8fd3e2
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7606,6 +7606,46 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_HUMIDITY_INFO = "lockscreen_weather_humidity_info";
 
         /**
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION_ENABLED = "charging_animation_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION_STYLE = "charging_animation_style";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_RIPPLE_OPACITY = "charging_ripple_opacity";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_GLOW_INTENSITY = "charging_glow_intensity";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_ARC_COUNT = "charging_arc_count";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_COLOR_MODE = "charging_color_mode";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_SHOW_ON_AOD = "charging_show_on_aod";
+
+        /**
+         * @hide
+         */
+        public static final String CHARGING_SHOW_ON_LOCKSCREEN = "charging_show_on_lockscreen";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
