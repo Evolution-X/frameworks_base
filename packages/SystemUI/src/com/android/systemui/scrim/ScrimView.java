37b5e259635d6a0635cf1eb5cad1ce02cea66646
Ghosuto <clash.raja10@gmail.com>
2025-11-30 12:53:03 +0000

    SystemUI: Allow toggle single tone QS background [1/2]
    - Thanks rmp22 for idea :)
    
    Change-Id: I7fc4415d61c9d1ef960d491d41ba5900c8826d21
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/scrim/ScrimView.java
+++ packages/SystemUI/src/com/android/systemui/scrim/ScrimView.java
@@ -32,6 +32,9 @@ import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -159,6 +162,14 @@ public class ScrimView extends View {
         executeOnExecutor(() -> {
             super.setClickable(clickable);
         });
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility
+            (isNotificationScrim() 
+                ? View.GONE 
+                : visibility);
     }
 
     /**
@@ -333,6 +344,7 @@ public class ScrimView extends View {
 
     public void setScrimName(String scrimName) {
         mScrimName = scrimName;
+        setVisibility(getVisibility());
     }
 
     @Override
@@ -385,6 +397,10 @@ public class ScrimView extends View {
      * Blur the view with the specific blur radius or clear any blurs if the radius is 0
      */
     public void setBlurRadius(float blurRadius) {
+        if (isNotificationScrim()) {
+            setRenderEffect(null);
+            return;
+        }
         if (blurRadius > 0) {
             debugLog("Apply blur RenderEffect to ScrimView " + mScrimName + " for radius "
                     + blurRadius);
@@ -402,5 +418,15 @@ public class ScrimView extends View {
         if (isDebugLoggable) {
             Log.d(TAG, logMsg);
         }
+    }
+
+    private boolean isSingleQsToneEnabled() {
+        return Settings.System.getIntForUser(getContext().getContentResolver(),
+            Settings.System.SINGLE_QS_TONE_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+    }
+
+    private boolean isNotificationScrim() {
+        return isSingleQsToneEnabled() && TextUtils.equals(
+            mScrimName, "notifications_scrim");
     }
 }
