9a2678c3427b71b2a8a7f73f27bb2e3e09b1f39c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-25 13:15:12 +0800

    core: Add play integrity spoofing
    Change-Id: I11c1f9e2f51d999aaf451264c328d434f8e54602
    Co-authored-by: osm0sis <osm0sis@outlook.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -125,6 +125,7 @@ import android.ravenwood.annotation.RavenwoodRedirect;
 import android.ravenwood.annotation.RavenwoodRedirectionClass;
 import android.ravenwood.annotation.RavenwoodReplace;
 import android.ravenwood.annotation.RavenwoodSupported.RavenwoodProvidingImplementation;
+import android.security.pif.PlayIntegritySpoofService;
 import android.system.ErrnoException;
 import android.system.Os;
 import android.system.OsConstants;
@@ -1004,6 +1005,11 @@ public class ApplicationPackageManager extends PackageManager {
         //    * IPC-retrieved system features (lazily cached, requires per-feature IPC)
         // TODO(b/375000483): Refactor all of this logic, including flag queries, into
         // the SystemFeaturesCache class after initial rollout and validation.
+        PlayIntegritySpoofService pifService = PlayIntegritySpoofService.getInstance();
+        if (pifService.shouldSpoofPhotos(ActivityThread.currentPackageName())) {
+            return pifService.hasSystemFeature(name, version);
+        }
+
         Boolean maybeHasSystemFeature = RoSystemFeatures.maybeHasFeature(name, version);
         if (maybeHasSystemFeature != null) {
             return maybeHasSystemFeature;
