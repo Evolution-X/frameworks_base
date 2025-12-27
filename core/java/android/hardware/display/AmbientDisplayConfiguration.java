478f2486165638637eb1f0e9afaa1c76008d5510
Pranav Vashi <neobuddy89@gmail.com>
2025-12-27 10:46:58 +0530

    EdgeLight: Make it work independent of ambient display notifications
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/851
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -94,6 +94,7 @@ public class AmbientDisplayConfiguration {
         return pulseOnNotificationEnabled(user)
                 || pulseOnLongPressEnabled(user)
                 || alwaysOnEnabled(user)
+                || edgeLightEnabled(user)
                 || isAmbientTickerEnabled(user)
                 || wakeLockScreenGestureEnabled(user)
                 || wakeDisplayGestureEnabled(user)
@@ -144,6 +145,12 @@ public class AmbientDisplayConfiguration {
     /** @hide */
     public boolean dozePickupSensorAvailable() {
         return mContext.getResources().getBoolean(R.bool.config_dozePulsePickup);
+    }
+
+    /** @hide */
+    public boolean edgeLightEnabled(int user) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.EDGE_LIGHT_ENABLED, 0, user) != 0;
     }
 
     /** @hide */
