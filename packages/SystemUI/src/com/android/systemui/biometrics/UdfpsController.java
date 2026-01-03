1d7e4fabe34470f98e89adec1ece1ef8c2cc3445
Mashopy <eliasgheeraert@gmail.com>
2026-01-03 01:30:42 +0100

    SystemUI: Reverse MediaTek udfps dimlayer changes
    The current impl for udfps framework dimming mostly targets OPlus Qualcomm devices, making it
    unsuitable for MediaTek devices lacking LHBM supports.
    
    Due to how GHBM is handled by MediaTek on BSP (HWC + RenderEngine + SysUI), it causes multiple issues
    on them, notably the infamous flashbang behavior with GHBM as MediaTek HWC expect multiple DimLayers that dynamically parse
    alpha based on the current brightness value and then order renderengine to dither it to not trigger a flashbang.
    
    This behavior has been mostly reversed from PRIZE MtkSystemUI apk from LAVA and tested on Nothing Phone (2a).
    
    TODO: Test on non-PRIZE devices and possibly let other devices change the alpha smoother point.
    Change-Id: I907d9075c79ab4af5b82d3781291d97c1eab17b1

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -79,6 +79,7 @@ import com.android.systemui.Dumpable;
 import com.android.systemui.Flags;
 import com.android.systemui.animation.ActivityTransitionAnimator;
 import com.android.systemui.biometrics.AuthController;
+import com.android.systemui.biometrics.MtkUdfpsScrimController;
 import com.android.systemui.biometrics.dagger.BiometricsBackground;
 import com.android.systemui.biometrics.domain.interactor.UdfpsOverlayInteractor;
 import com.android.systemui.biometrics.shared.model.UdfpsOverlayParams;
@@ -240,6 +241,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean mOnFingerDown;
     private boolean mAttemptedToDismissKeyguard;
     private final Set<Callback> mCallbacks = new HashSet<>();
+
+    private boolean mUseMtkGhbmDimming;
 
     private UdfpsAnimation mUdfpsAnimation;
     private boolean mKeyguardCallbackRegistered = false;
@@ -874,6 +877,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mWakefulnessLifecycle.addObserver(mWakefulnessLifecycleObserver);
         }
 
+        mUseMtkGhbmDimming = mContext.getResources().getBoolean(
+            com.android.systemui.res.R.bool.config_udfpsMtkGhbmDimming);
+
         if (com.android.internal.util.evolution.Utils.isPackageInstalled(mContext,
                 "org.evolution.udfps.animations")) {
             updateUdfpsAnimation();
@@ -985,6 +991,15 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             if (oldView != null) {
                 onFingerUp(mOverlay.getRequestId(), oldView);
             }
+
+            // Ensure HBM views are removed from WindowManager
+            if (mUseMtkGhbmDimming) {
+                View hbmView = mOverlay.getHbmView();
+                if (hbmView != null) {
+                    hbmView.setVisibility(View.GONE);
+                }
+            }
+
             final boolean removed = mOverlay.hide();
             mKeyguardViewManager.hideAlternateBouncer(true);
             hideUdfpsAnimation();
@@ -1211,6 +1226,30 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mOnFingerDown = true;
         mFingerprintManager.onPointerDown(requestId, mSensorProps.sensorId, pointerId, x, y,
                 minor, major, orientation, time, gestureStart, isAod);
+
+        if (mUseMtkGhbmDimming && mOverlay != null) {
+            View hbmView = mOverlay.getHbmView(); // Ensure UdfpsControllerOverlay exposes this
+            WindowManager.LayoutParams hbmParams = mOverlay.getHbmLayoutParamsFull();
+
+            if (hbmView != null && hbmParams != null) {
+                MtkUdfpsScrimController mtkController = MtkUdfpsScrimController.getInstance(mContext);
+
+                int brightness = mtkController.getSystemBrightness();
+                float alpha = mtkController.calculateAlpha(brightness);
+
+                Log.d(TAG, "UDFPS Scrim: Brightness=" + brightness + " CalculatedAlpha=" + alpha);
+
+                if (hbmView.getVisibility() != View.VISIBLE) {
+                    hbmView.setVisibility(View.VISIBLE);
+                }
+
+                if (Math.abs(hbmParams.alpha - alpha) > 0.001f) {
+                    hbmParams.alpha = alpha;
+                    mWindowManager.updateViewLayout(hbmView, hbmParams);
+                }
+            }
+        }
+
         Trace.endAsyncSection("UdfpsController.e2e.onPointerDown", 0);
 
         final View view = mOverlay.getTouchOverlay();
@@ -1271,6 +1310,14 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             hideUdfpsAnimation();
         }
         mOnFingerDown = false;
+
+        if (mUseMtkGhbmDimming && mOverlay != null) {
+            View hbmView = mOverlay.getHbmView();
+            if (hbmView != null) {
+                hbmView.setVisibility(View.GONE);
+            }
+        }
+
         unconfigureDisplay(view);
         cancelAodSendFingerUpAction();
     }
