cef858f344e4bb2ad0d9c0a00438a709a539adaf
Ghosuto <clash.raja10@gmail.com>
2026-02-03 19:24:57 +0000

    SystemUI: Refactor Lockscreen Media art
    - AOD (Always-On Display) Support with Adjustable AOD dim level
    - Pixelation Filter
    - Only shows media art in portrait mode
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7148,11 +7148,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String AMBIENT_MEDIA_ART_ENABLED = "ambient_media_art_enabled";
-
-        /**
-         * @hide
-         */
         public static final String LS_MEDIA_ART_FILTER = "ls_media_art_filter";
 
         /**
@@ -7164,6 +7159,21 @@ public final class Settings {
          * @hide
          */
         public static final String LS_MEDIA_ART_BLUR_LEVEL = "ls_media_art_blur_level";
+
+        /**
+         * @hide
+         */
+        public static final String LS_MEDIA_ART_PIXEL_SIZE = "ls_media_art_pixel_size";
+
+        /**
+         * @hide
+         */
+        public static final String LS_MEDIA_ART_AOD_ENABLED = "ls_media_art_aod_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String LS_MEDIA_ART_AOD_DIM_LEVEL = "ls_media_art_aod_dim_level";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
