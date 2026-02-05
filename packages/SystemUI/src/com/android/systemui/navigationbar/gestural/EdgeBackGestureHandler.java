696d37573bfd007de0b826f38146c4c718dacbbd
Wu Shaokang <wu.shaokang@zte.com.cn>
2026-02-05 17:04:02 +0800

    Fix NavBar haptic feedback after Bluetooth device disconnect
    After disconnecting a Bluetooth controller or trackpad, the virtual key
    haptic feedback for the navigation bar becomes disabled. This causes
    subsequent presses of the Back and Home buttons to provide no haptic
    feedback to the user.
    
    The issue occurs because when the last trackpad is removed from
    mTrackpadsConnected, the update() method is called but the state change
    callback is not triggered, leaving the navigation bar's haptic feedback
    disabled.
    
    This fix ensures that mStateChangeCallback.run() is invoked after
    update() when the trackpad list becomes empty, properly re-enabling
    haptic feedback for navigation bar buttons.
    
    Test:
    
    Connect/disconnect Bluetooth controller, verify navigation bar haptic
    feedback works after disconnect
    
    Change-Id: Icf6e7526f7a06784e0e4d63b85c0879e36c09bc5

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -448,6 +448,9 @@ public class EdgeBackGestureHandler implements TunerService.Tunable {
                 mTrackpadsConnected.remove(deviceId);
                 if (mTrackpadsConnected.isEmpty()) {
                     update();
+                    if (mStateChangeCallback != null) {
+                        mStateChangeCallback.run();
+                    }
                 }
             });
         }
