d2589ec68187489f993018a251ef99700d85f17e
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-09 08:08:43 +0800

    SystemUI: Add smart pixels support
    Change-Id: I003b0ef78309bbf18f0c7bf3d877102108f9931f
    Co-authored-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Co-authored-by: Joe Maples <joe@frap129.org>
    Co-authored-by: Sergii Pylypenko <x.pelya.x@gmail.com>
    Co-authored-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -54,6 +54,7 @@ import com.android.systemui.media.taptotransfer.receiver.MediaTttChipControllerR
 import com.android.systemui.media.taptotransfer.sender.MediaTttSenderCoordinator
 import com.android.systemui.mediaprojection.taskswitcher.MediaProjectionTaskSwitcherCoreStartable
 import com.android.systemui.shortcut.ShortcutKeyDispatcher
+import com.android.systemui.smartpixel.ui.SmartPixelManager
 import com.android.systemui.statusbar.ImmersiveModeConfirmation
 import com.android.systemui.statusbar.chips.ui.viewmodel.OngoingActivityChipsRefiner
 import com.android.systemui.statusbar.gesture.GesturePointerEventListener
@@ -335,6 +336,13 @@ abstract class SystemUICoreStartableModule {
     @ClassKey(SysUIKeyGestureEventInitializer::class)
     abstract fun bindSysUIKeyGestureEventInitializer(
         keyGestureEventInitializer: SysUIKeyGestureEventInitializer
+    ): CoreStartable
+
+    @Binds
+    @IntoMap
+    @ClassKey(SmartPixelManager::class)
+    abstract fun bindSmartPixelManager(
+        impl: SmartPixelManager
     ): CoreStartable
 
     @Binds
