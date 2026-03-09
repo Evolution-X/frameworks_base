59f318cea36468a30f9eaafd8eeaa5ca568d8355
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-09 08:08:43 +0800

    SystemUI: Add smart pixels support
    Change-Id: I003b0ef78309bbf18f0c7bf3d877102108f9931f
    Co-authored-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Co-authored-by: Joe Maples <joe@frap129.org>
    Co-authored-by: Sergii Pylypenko <x.pelya.x@gmail.com>
    Co-authored-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14197,6 +14197,18 @@ public final class Settings {
         public static final String PULSE_CAPTURE_MODE = "pulse_capture_mode";
 
         /**
+         * Whether to enable Smart Pixels
+         * @hide
+         */
+        public static final String SMART_PIXEL_FILTER_ENABLED = "smart_pixel_filter_enabled";
+
+        /**
+         * Smart Pixels percentage
+         * @hide
+         */
+        public static final String SMART_PIXEL_FILTER_PERCENT = "smart_pixel_filter_percent";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
