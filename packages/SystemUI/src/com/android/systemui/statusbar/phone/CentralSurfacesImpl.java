060d4fb9b7d98d6ed4f86a4473523bbc9999a45c
Ghosuto <clash.raja10@gmail.com>
2026-03-17 15:08:23 +0000

    SystemUI: Implement device idle manager [1/2]
    - Introduce a per-app idle management system that enforces background
    process restrictions when the screen is off
    
    - The screen-off/on integration points in CentralSurfacesImpl are
    inspired by Descendant OS
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -108,6 +108,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.ActivityIntentHelper;
+import com.android.systemui.lunaris.LunarisIdleManager;
 import com.android.systemui.AutoReinflateContainer;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.DejankUtils;
@@ -2731,6 +2732,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            if (Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.IDLE_MANAGER, 1) == 1) {
+                LunarisIdleManager.initManager(mContext);
+                LunarisIdleManager mgr = LunarisIdleManager.getInstance();
+                if (mgr != null && !mgr.isRunning()) {
+                    mgr.executeManager();
+                }
+            }
         }
 
         @Override
@@ -2754,6 +2763,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            LunarisIdleManager mgr = LunarisIdleManager.getInstance();
+            if (mgr != null) {
+                mgr.haltManager();
+            }
         }
 
         /**
