16a548d1e87c072268334386bf9c156cddf860e9
hiroshi <hiroshi@tanuki.page>
2026-03-18 18:33:15 -0400

    base: Empty accessibility service list when hiding app list
    Some apps like to block usage when there's an acessibility service enabled.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -109,6 +109,7 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.util.Preconditions;
 
 import com.android.internal.util.evolution.HideDeveloperStatusUtils;
+import com.android.internal.util.evolution.HideAppListUtils;
 
 import java.io.IOException;
 import java.lang.annotation.ElementType;
@@ -3733,6 +3734,10 @@ public final class Settings {
                 final @CanBeCURRENT @UserIdInt int userId) {
             if (HideDeveloperStatusUtils.shouldHideDevStatus(cr, cr.getPackageName(), name)) {
                 return "0" /* Disabled */;
+            }
+            if (name.equals(Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES) 
+            && HideAppListUtils.shouldHideAppList(cr, cr.getPackageName())) {
+                return "";
             }
             final boolean isSelf = (userId == UserHandle.myUserId());
             final AttributionSource attributionSource = cr.getAttributionSource();
@@ -8480,6 +8485,11 @@ public final class Settings {
             if (HideDeveloperStatusUtils.shouldHideDevStatus(resolver, resolver.getPackageName(), name)) {
                 return "0" /* Disabled */;
             }
+
+            if (name.equals(Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES) 
+            && HideAppListUtils.shouldHideAppList(resolver, resolver.getPackageName())) {
+                return "";
+            }
             return getStringForUser(resolver, name, resolver.getUserId());
         }
 
@@ -8489,6 +8499,10 @@ public final class Settings {
                 @CanBeCURRENT @UserIdInt int userId) {
             if (HideDeveloperStatusUtils.shouldHideDevStatus(resolver, resolver.getPackageName(), name)) {
                 return "0" /* Disabled */;
+            }
+            if (name.equals(Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES) 
+            && HideAppListUtils.shouldHideAppList(resolver, resolver.getPackageName())) {
+                return "";
             }
             if (MOVED_TO_GLOBAL.contains(name)) {
                 Log.w(TAG, "Setting " + name + " has moved from android.provider.Settings.Secure"
@@ -23053,6 +23067,10 @@ public final class Settings {
             ContentResolver resolver = getContentResolver();
             if (HideDeveloperStatusUtils.shouldHideDevStatus(resolver, resolver.getPackageName(), name)) {
                 return "0" /* Disabled */;
+            }
+            if (name.equals(Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES) 
+            && HideAppListUtils.shouldHideAppList(resolver, resolver.getPackageName())) {
+                return "";
             }
             return sNameValueCache.getStringForUser(resolver, name, resolver.getUserId());
         }
