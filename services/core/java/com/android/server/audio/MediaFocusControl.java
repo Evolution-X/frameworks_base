04e6ee4559f8364c24e5f8596f1dee2d33f33b40
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-21 22:46:36 +0800

    Adding multi-media focus support
    [joeyhuab] Adapt to A17
    
    Change-Id: I0f9dbe4a8c12dea70e0fb8578e76690c13a52f14

--- services/core/java/com/android/server/audio/MediaFocusControl.java
+++ services/core/java/com/android/server/audio/MediaFocusControl.java
@@ -100,6 +100,10 @@ public class MediaFocusControl implements PlayerFocusEnforcer {
     private final @NonNull PlayerFocusEnforcer mFocusEnforcer;
     private boolean mMultiAudioFocusEnabled = false;
 
+    boolean isMultiAudioFocusEnabled() {
+        return mMultiAudioFocusEnabled;
+    }
+
     private final Object mExtFocusChangeLock = new Object();
     @GuardedBy("mExtFocusChangeLock")
     private long mExtFocusChangeCounter;
@@ -643,7 +647,7 @@ public class MediaFocusControl implements PlayerFocusEnforcer {
                 }
             } else {
                 for (FocusRequester multifr : mMultiAudioFocusList) {
-                    if (isLockedFocusOwner(multifr)) {
+                    if (isLockedFocusOwner(multifr) || mFocusStack.empty()) {
                         multifr.handleFocusGain(AudioManager.AUDIOFOCUS_GAIN);
                     }
                 }
