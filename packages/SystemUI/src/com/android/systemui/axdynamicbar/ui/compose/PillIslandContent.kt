a81dbc481197bcf934092a0076d77cd0f11176aa
Ghosuto <clash.raja10@gmail.com>
2026-04-02 12:09:24 +0000

    SystemUI: DynamicBar: Improve chip padding and size
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/compose/PillIslandContent.kt
+++ packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/compose/PillIslandContent.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -1114,10 +1115,12 @@ private fun MediaTitleText(event: IslandEvent.Media, modifier: Modifier, overrid
         style = PillPrimary,
         maxLines = 1,
         overflow = TextOverflow.Clip,
-        modifier = modifier.basicMarquee(
-            initialDelayMillis = 3_000,
-            repeatDelayMillis = 5_000,
-        ),
+        modifier = modifier
+            .widthIn(max = 90.dp)
+            .basicMarquee(
+                initialDelayMillis = 3_000,
+                repeatDelayMillis = 5_000,
+            ),
     )
 }
 
