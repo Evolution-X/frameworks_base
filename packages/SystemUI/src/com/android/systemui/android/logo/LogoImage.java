e1e152167d55f1e55d0f96ebb5708725834f3020
Ghosuto <clash.raja10@gmail.com>
2026-04-16 19:00:58 +0000

    SystemUI: User selected custom statusbar logo [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/android/logo/LogoImage.java
+++ packages/SystemUI/src/com/android/systemui/android/logo/LogoImage.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2018-2025 crDroid Android Project
  * Copyright (C) 2018-2019 AICP
+ * Copyright (C) 2024-2026 Lunaris AOSP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,22 +18,30 @@
 
 package com.android.systemui.android.logo;
 
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.ColorStateList;
 import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.os.UserManager;
 import android.provider.Settings;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.android.settingslib.Utils;
+import com.android.settingslib.drawable.CircleFramedDrawable;
 
 import com.android.systemui.Dependency;
 import com.android.systemui.plugins.DarkIconDispatcher;
@@ -42,6 +51,10 @@ import com.android.systemui.res.R;
 import java.util.ArrayList;
 
 public abstract class LogoImage extends ImageView implements DarkReceiver {
+
+    private static final String TAG = "LogoImage";
+
+    public static final int LOGO_STYLE_CUSTOM = 34;
 
     private Context mContext;
 
@@ -54,7 +67,16 @@ public abstract class LogoImage extends ImageView implements DarkReceiver {
     private int mLogoColor;
     private int mLogoColorCustom;
 
+    private String mCustomImagePath;
+    private String mCurrentCustomImagePath;
+    private boolean mCustomImageLoaded = false;
+    private Drawable mCustomImageDrawable = null;
+
     private ContentObserver mSettingsObserver;
+
+    private boolean mUserUnlocked = false;
+    private BroadcastReceiver mUserUnlockedReceiver;
+    private boolean mReceiverRegistered = false;
 
     public LogoImage(Context context) {
         this(context, null);
@@ -71,51 +93,119 @@ public abstract class LogoImage extends ImageView implements DarkReceiver {
 
     protected abstract boolean isLogoVisible();
 
+    private boolean isUserUnlocked() {
+        try {
+            UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+            return um != null && um.isUserUnlocked();
+        } catch (Exception e) {
+            Log.w(TAG, "Could not query UserManager lock state, assuming locked", e);
+            return false;
+        }
+    }
+
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        if (mAttached)
-            return;
-
+        if (mAttached) return;
         mAttached = true;
+
+        mUserUnlocked = isUserUnlocked();
 
         mSettingsObserver = new ContentObserver(new Handler()) {
             @Override
             public void onChange(boolean selfChange) {
                 super.onChange(selfChange);
-                updateSettings();
-
+                if (mUserUnlocked) {
+                    updateSettings();
+                } else {
+                    Log.d(TAG, "Settings changed but user is locked, deferring");
+                }
             }
         };
-        mContext.getContentResolver().registerContentObserver(
+
+        final ContentResolver resolver = mContext.getContentResolver();
+        resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.STATUS_BAR_LOGO),
                 false, mSettingsObserver, UserHandle.USER_ALL);
-        mContext.getContentResolver().registerContentObserver(
+        resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.STATUS_BAR_LOGO_POSITION),
                 false, mSettingsObserver, UserHandle.USER_ALL);
-        mContext.getContentResolver().registerContentObserver(
+        resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.STATUS_BAR_LOGO_STYLE),
                 false, mSettingsObserver, UserHandle.USER_ALL);
-        mContext.getContentResolver().registerContentObserver(
+        resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.STATUS_BAR_LOGO_COLOR),
                 false, mSettingsObserver, UserHandle.USER_ALL);
-        mContext.getContentResolver().registerContentObserver(
+        resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.STATUS_BAR_LOGO_COLOR_PICKER),
                 false, mSettingsObserver, UserHandle.USER_ALL);
-        updateSettings();
+        resolver.registerContentObserver(
+                Settings.System.getUriFor(Settings.System.STATUS_BAR_LOGO_CUSTOM_IMAGE_URI),
+                false, mSettingsObserver, UserHandle.USER_ALL);
 
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
+
+        if (mUserUnlocked) {
+            updateSettings();
+        } else {
+            Log.w(TAG, "CE storage locked on attach, deferring settings load until unlock");
+            setImageDrawable(null);
+            setVisibility(View.GONE);
+            registerUserUnlockedReceiver();
+        }
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        if (!mAttached)
-            return;
-
+        if (!mAttached) return;
         mAttached = false;
+
         mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(this);
+
+        unregisterUserUnlockedReceiver();
+
+        mCustomImageDrawable = null;
+    }
+
+    private void registerUserUnlockedReceiver() {
+        if (mReceiverRegistered) return;
+
+        mUserUnlockedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (!Intent.ACTION_USER_UNLOCKED.equals(intent.getAction())) return;
+
+                Log.d(TAG, "User unlocked, initialising logo settings");
+                mUserUnlocked = true;
+
+                unregisterUserUnlockedReceiver();
+
+                post(LogoImage.this::updateSettings);
+            }
+        };
+
+        try {
+            IntentFilter filter = new IntentFilter(Intent.ACTION_USER_UNLOCKED);
+            mContext.registerReceiver(mUserUnlockedReceiver, filter);
+            mReceiverRegistered = true;
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to register user unlocked receiver", e);
+            mUserUnlockedReceiver = null;
+        }
+    }
+
+    private void unregisterUserUnlockedReceiver() {
+        if (!mReceiverRegistered || mUserUnlockedReceiver == null) return;
+        try {
+            mContext.unregisterReceiver(mUserUnlockedReceiver);
+        } catch (Exception e) {
+            Log.w(TAG, "Error unregistering user unlocked receiver", e);
+        } finally {
+            mUserUnlockedReceiver = null;
+            mReceiverRegistered = false;
+        }
     }
 
     @Override
@@ -127,6 +217,10 @@ public abstract class LogoImage extends ImageView implements DarkReceiver {
     }
 
     public void updateLogo() {
+        if (mLogoStyle == LOGO_STYLE_CUSTOM) {
+            updateCustomLogo();
+            return;
+        }
         Drawable drawable = null;
         switch (mLogoStyle) {
             case 0:
@@ -234,6 +328,8 @@ public abstract class LogoImage extends ImageView implements DarkReceiver {
                 break;
         }
 
+        if (drawable == null) return;
+
         if (mLogoColor == 0) {
             drawable.setTint(mTintColor);
         } else if (mLogoColor == 1) {
@@ -245,22 +341,115 @@ public abstract class LogoImage extends ImageView implements DarkReceiver {
         setImageDrawable(drawable);
     }
 
+    private void updateCustomLogo() {
+        if (mCustomImagePath != null && !mCustomImagePath.equals(mCurrentCustomImagePath)) {
+            mCurrentCustomImagePath = mCustomImagePath;
+            mCustomImageLoaded = false;
+            mCustomImageDrawable = null;
+        }
+
+        if (!mCustomImageLoaded) {
+            loadCustomImage();
+        }
+
+        if (mCustomImageDrawable != null) {
+            clearColorFilter();
+            setImageTintList(null);
+            setImageDrawable(mCustomImageDrawable);
+        } else {
+            setImageDrawable(
+                    mContext.getResources().getDrawable(R.drawable.ic_android_logo));
+        }
+    }
+
+    private void loadCustomImage() {
+        if (mCurrentCustomImagePath == null || mCurrentCustomImagePath.isEmpty()) return;
+
+        if (!mUserUnlocked) {
+            Log.d(TAG, "Skipping custom image load - CE storage not yet available");
+            return;
+        }
+
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeFile(mCurrentCustomImagePath);
+            if (bitmap != null) {
+                int targetSize = (int) mContext.getResources()
+                        .getDimension(R.dimen.status_bar_system_icons_height);
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(
+                        bitmap, targetSize, targetSize, true);
+                try (java.io.ByteArrayOutputStream stream = new java.io.ByteArrayOutputStream()) {
+                    scaledBitmap.compress(Bitmap.CompressFormat.WEBP_LOSSLESS, 90, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    Bitmap compressedBitmap = BitmapFactory.decodeByteArray(
+                            byteArray, 0, byteArray.length);
+                    mCustomImageDrawable = new CircleFramedDrawable(compressedBitmap, targetSize);
+                    scaledBitmap.recycle();
+                    compressedBitmap.recycle();
+                    mCustomImageLoaded = true;
+                }
+            } else {
+                Log.w(TAG, "Failed to decode custom logo bitmap: " + mCurrentCustomImagePath);
+                mCustomImageLoaded = false;
+                mCustomImageDrawable = null;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading custom logo image", e);
+            mCustomImageLoaded = false;
+            mCustomImageDrawable = null;
+        } finally {
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
+        }
+    }
+
     public void updateSettings() {
-        mShowLogo = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_LOGO, 0, UserHandle.USER_CURRENT) != 0;
-        mLogoPosition = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_LOGO_POSITION, 0, UserHandle.USER_CURRENT);
-        mLogoStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_LOGO_STYLE, 0, UserHandle.USER_CURRENT);
-        mLogoColor = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_LOGO_COLOR, 0, UserHandle.USER_CURRENT);
-        mLogoColorCustom = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_LOGO_COLOR_PICKER, 0xff1a73e8, UserHandle.USER_CURRENT);
+        if (!mUserUnlocked) {
+            Log.d(TAG, "updateSettings() skipped - CE storage locked");
+            return;
+        }
+
+        try {
+            mShowLogo = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO, 0,
+                    UserHandle.USER_CURRENT) != 0;
+            mLogoPosition = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_POSITION, 0,
+                    UserHandle.USER_CURRENT);
+            mLogoStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_STYLE, 0,
+                    UserHandle.USER_CURRENT);
+            mLogoColor = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_COLOR, 0,
+                    UserHandle.USER_CURRENT);
+            mLogoColorCustom = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_COLOR_PICKER, 0xff1a73e8,
+                    UserHandle.USER_CURRENT);
+
+            String newCustomPath = Settings.System.getStringForUser(
+                    mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_CUSTOM_IMAGE_URI,
+                    UserHandle.USER_CURRENT);
+            if (newCustomPath != null && !newCustomPath.equals(mCustomImagePath)) {
+                mCustomImageLoaded = false;
+                mCustomImageDrawable = null;
+            }
+            mCustomImagePath = newCustomPath;
+
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading logo settings from CE storage", e);
+            setImageDrawable(null);
+            setVisibility(View.GONE);
+            return;
+        }
+
         if (!mShowLogo || !isLogoVisible()) {
             setImageDrawable(null);
             setVisibility(View.GONE);
             return;
         }
+
         updateLogo();
         setVisibility(View.VISIBLE);
     }
