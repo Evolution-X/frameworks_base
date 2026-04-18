9434ccb92898e1c63533dd737899d97da66ad589
Pranav Vashi <neobuddy89@gmail.com>
2026-04-18 07:53:13 +0530

    SystemUI: DynamicBar: Re-add notification and call event
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: TwistedVision518 <pranavpraveen0@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/axdynamicbar/shared/IslandActions.kt
+++ packages/SystemUI/src/com/android/systemui/axdynamicbar/shared/IslandActions.kt
@@ -9,6 +9,7 @@ val EVENT_TYPE_IDS: Map<Class<out IslandEvent>, String> =
         IslandEvent.PromotedOngoing::class.java to "promoted_ongoing",
         IslandEvent.Sports::class.java to "sports",
         IslandEvent.Media::class.java to "media",
+        IslandEvent.Call::class.java to "call",
         IslandEvent.Bluetooth::class.java to "bluetooth",
         IslandEvent.Hotspot::class.java to "hotspot",
         IslandEvent.Charging::class.java to "charging",
@@ -50,5 +51,7 @@ interface IslandActions {
     fun killApp(taskId: Int)
     fun onNotificationInteraction(eventId: String)
     fun onNotificationInteractionEnd(eventId: String)
+    fun onNotificationAlertInteractionStart()
+    fun onNotificationAlertInteractionEnd()
     fun launchNotificationDismissingKeyguard(event: IslandEvent.Notification)
 }
