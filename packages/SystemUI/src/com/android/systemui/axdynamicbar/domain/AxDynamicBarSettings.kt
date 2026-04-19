87e44d5dd0216f460d9674c992edf2f1099da2c3
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-19 16:01:25 +0800

    SystemUI: DynamicBar: Cleaning up ux & animations enhance
    removing unncecessary events | rewiring to aosp status bar chip
    
    Change-Id: Ib1c2bbcd09c871ff14a2c18f485bee22874679a1
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/axdynamicbar/domain/AxDynamicBarSettings.kt
+++ packages/SystemUI/src/com/android/systemui/axdynamicbar/domain/AxDynamicBarSettings.kt
@@ -4,12 +4,10 @@ import android.database.ContentObserver
 import android.os.Handler
 import android.os.UserHandle
 import android.provider.Settings
-import android.provider.Settings.Global
 import com.android.systemui.axdynamicbar.model.IslandEvent
 import com.android.systemui.axdynamicbar.shared.EVENT_TYPE_IDS
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.dagger.qualifiers.Main
-import com.android.systemui.util.settings.GlobalSettings
 import com.android.systemui.util.settings.SecureSettings
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,7 +19,6 @@ import org.json.JSONArray
 class AxDynamicBarSettings @Inject constructor(
     @Main private val mainHandler: Handler,
     private val secureSettings: SecureSettings,
-    private val globalSettings: GlobalSettings,
     private val context: android.content.Context,
 ) {
     companion object {
@@ -30,7 +27,6 @@ class AxDynamicBarSettings @Inject constructor(
         const val KEY_KEYGUARD_ENABLED = "ax_dynamic_bar_keyguard_enabled"
         const val KEY_KEYGUARD_MUSIC_PILL_ENABLED = "ax_dynamic_bar_keyguard_music_pill"
         const val KEY_KEYGUARD_BATTERY_CHIP_MODE = "ax_dynamic_bar_keyguard_battery_chip_mode"
-        const val KEY_COMPACT_NOTIFICATIONS = "ax_dynamic_bar_compact_notifications"
     }
 
     private val contentResolver = context.contentResolver
@@ -46,12 +42,6 @@ class AxDynamicBarSettings @Inject constructor(
 
     private val _keyguardBatteryChipMode = MutableStateFlow(1)
     val keyguardBatteryChipMode: StateFlow<Int> = _keyguardBatteryChipMode.asStateFlow()
-
-    private val _compactNotifications = MutableStateFlow(true)
-    val compactNotifications: StateFlow<Boolean> = _compactNotifications.asStateFlow()
-
-    private val _isHeadsUpEnabled = MutableStateFlow(true)
-    val isHeadsUpEnabled: StateFlow<Boolean> = _isHeadsUpEnabled.asStateFlow()
 
     private val _useWaveformSeekBar = MutableStateFlow(false)
     val useWaveformSeekBar: StateFlow<Boolean> = _useWaveformSeekBar.asStateFlow()
@@ -106,17 +96,6 @@ class AxDynamicBarSettings @Inject constructor(
             settingsObserver,
             UserHandle.USER_ALL,
         )
-        secureSettings.registerContentObserverForUserSync(
-            KEY_COMPACT_NOTIFICATIONS,
-            false,
-            settingsObserver,
-            UserHandle.USER_ALL,
-        )
-        globalSettings.registerContentObserverSync(
-            Global.HEADS_UP_NOTIFICATIONS_ENABLED,
-            false,
-            settingsObserver,
-        )
         contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.MEDIA_WAVEFORM_SEEKBAR),
             false,
@@ -129,7 +108,6 @@ class AxDynamicBarSettings @Inject constructor(
         if (!initialized) return
         initialized = false
         secureSettings.getContentResolver().unregisterContentObserver(settingsObserver)
-        globalSettings.getContentResolver().unregisterContentObserver(settingsObserver)
         contentResolver.unregisterContentObserver(settingsObserver)
     }
 
@@ -142,10 +120,6 @@ class AxDynamicBarSettings @Inject constructor(
             secureSettings.getIntForUser(KEY_KEYGUARD_BATTERY_CHIP_MODE, 1, UserHandle.USER_CURRENT)
         _isKeyguardMusicPillEnabled.value =
             secureSettings.getIntForUser(KEY_KEYGUARD_MUSIC_PILL_ENABLED, 0, UserHandle.USER_CURRENT) == 1
-        _compactNotifications.value =
-            secureSettings.getIntForUser(KEY_COMPACT_NOTIFICATIONS, 1, UserHandle.USER_CURRENT) == 1
-        _isHeadsUpEnabled.value =
-            globalSettings.getInt(Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1) == 1
         _useWaveformSeekBar.value =
             Settings.System.getIntForUser(
                 contentResolver, Settings.System.MEDIA_WAVEFORM_SEEKBAR, 0, UserHandle.USER_CURRENT,) == 1
@@ -167,7 +141,4 @@ class AxDynamicBarSettings @Inject constructor(
         val typeId = EVENT_TYPE_IDS[event::class.java] ?: return true
         return typeId !in _disabledEventTypes.value
     }
-
-    fun isNotificationEventsActive(): Boolean =
-        _isEnabled.value && "notification" !in _disabledEventTypes.value
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/CommonVisualInterruptionSuppressors.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/CommonVisualInterruptionSuppressors.kt
@@ -46,7 +46,6 @@ import android.service.notification.Flags
 import com.android.internal.logging.UiEvent
 import com.android.internal.logging.UiEventLogger
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage
-import com.android.systemui.axdynamicbar.domain.AxDynamicBarSettings
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.plugins.statusbar.StatusBarStateController
 import com.android.systemui.settings.UserTracker
@@ -113,15 +112,6 @@ class PeekDisabledSuppressor(
 
         observer.onChange(/* selfChange= */ true)
     }
-}
-
-class PeekAxDynamicBarSuppressor(
-    private val settings: AxDynamicBarSettings,
-) : VisualInterruptionCondition(
-    types = setOf(PEEK),
-    reason = "suppressed by AxDynamicBar"
-) {
-    override fun shouldSuppress(): Boolean = settings.isNotificationEventsActive()
 }
 
 class PulseDisabledSuppressor(
