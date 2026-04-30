5cf9d97dfb29b9de9bfd342c3ee19431981a84e8
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-01 06:10:37 +0800

    services: Optimizing wallpaper zoom
    Change-Id: I1549ec25043b749d86d3a806e83f6004090cd96d
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/wm/Session.java
+++ services/core/java/com/android/server/wm/Session.java
@@ -65,6 +65,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.permission.PermissionManager;
 import android.text.TextUtils;
+import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.Slog;
 import android.view.IWindow;
@@ -128,6 +129,9 @@ class Session extends IWindowSession.Stub implements IBinder.DeathRecipient {
     protected String mPackageName;
     private String mRelayoutTag;
     final boolean mSetsUnrestrictedKeepClearAreas;
+    private final ArrayMap<IBinder, Float> mPendingWallpaperZoomOut = new ArrayMap<>();
+    private final Runnable mApplyPendingWallpaperZoomOut = this::applyPendingWallpaperZoomOut;
+    private boolean mApplyWallpaperZoomOutPending;
 
     public Session(WindowManagerService service, IWindowSessionCallback callback) {
         this(service, callback, Binder.getCallingPid(), Binder.getCallingUid());
@@ -632,10 +636,30 @@ class Session extends IWindowSession.Stub implements IBinder.DeathRecipient {
                     + zoom);
         }
         synchronized (mService.mGlobalLock) {
+            mPendingWallpaperZoomOut.put(window, zoom);
+            if (!mApplyWallpaperZoomOutPending) {
+                mApplyWallpaperZoomOutPending = true;
+                mService.mH.post(mApplyPendingWallpaperZoomOut);
+            }
+        }
+    }
+
+    private void applyPendingWallpaperZoomOut() {
+        synchronized (mService.mGlobalLock) {
+            mApplyWallpaperZoomOutPending = false;
+            final int size = mPendingWallpaperZoomOut.size();
+            if (size == 0) {
+                return;
+            }
+            final ArrayMap<IBinder, Float> pending = new ArrayMap<>(mPendingWallpaperZoomOut);
+            mPendingWallpaperZoomOut.clear();
             final long ident = Binder.clearCallingIdentity();
             try {
-                actionOnWallpaper(window, (wpController, windowState) ->
-                        wpController.setWallpaperZoomOut(windowState, zoom));
+                for (int i = 0; i < size; i++) {
+                    final float zoom = pending.valueAt(i);
+                    actionOnWallpaper(pending.keyAt(i), (wpController, windowState) ->
+                            wpController.setWallpaperZoomOut(windowState, zoom));
+                }
             } finally {
                 Binder.restoreCallingIdentity(ident);
             }
