67f57063b2d27511011338f9c78602d53e33e294
Pranav Vashi <neobuddy89@gmail.com>
2026-05-03 08:50:12 +0530

    Pulse: Add subtle haptics on bass spikes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14125,6 +14125,11 @@ public final class Settings {
         public static final String PULSE_RENDERER = "pulse_renderer";
 
         /**
+         * @hide
+         */
+        public static final String PULSE_BASS_HAPTICS = "pulse_bass_haptics";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
