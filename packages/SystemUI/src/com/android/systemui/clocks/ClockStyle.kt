5f31b2f9822dbbb9b28658b26d8b8e8fc52c30bf
Pranav Vashi <neobuddy89@gmail.com>
2026-06-09 11:49:17 +0530

    SystemUI: ClockStyles: Remove setting center layout programmatically
    * This is not required. Layout xml itself should center the clock which we do for most of them.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.kt
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.kt
@@ -371,9 +371,6 @@ class ClockStyle @JvmOverloads constructor(
                 view.findViewById<TextView?>(R.id.device_name)?.text =
                     UserProfileUtils.getDeviceName()
 
-                if (view is LinearLayout) {
-                    view.gravity = if (isCenterClock(clockStyle)) Gravity.CENTER else Gravity.START
-                }
                 updateClockAppearance()
                 updateClockFrameMargin()
             }
@@ -564,8 +561,6 @@ class ClockStyle @JvmOverloads constructor(
         }
     }
 
-    private fun isCenterClock(style: Int): Boolean = CENTER_CLOCKS.contains(style)
-
     private fun isNoColorClock(style: Int): Boolean = NO_COLOR_CLOCKS.contains(style)
 
     companion object {
@@ -656,11 +651,6 @@ class ClockStyle @JvmOverloads constructor(
             R.layout.keyguard_clock_galada,         // 83
             R.layout.keyguard_clock_cos1,           // 84
             R.layout.keyguard_clock_cos2,           // 85
-        )
-
-        private val CENTER_CLOCKS = hashSetOf(
-            3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-            20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33
         )
 
         private val NO_COLOR_CLOCKS = hashSetOf(1, 2, 25, 26, 45)
