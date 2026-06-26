ce70129aad3b8e76d9115ca93b30100e36cea0b8
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-26 15:11:44 +0800

    core: adding axion sandbox support
    Change-Id: Ia754feadff534a29c08306f57afbc860ea8ef70b
    
    credit
    HMA OSS | Furkan Karcıoğlu
    
    [joeyhuab] Adapt to A17
    
    Co-authored-by: Furkan Karcıoğlu <45714956+frknkrc44@users.noreply.github.com>
    Co-auhtored-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Change-Id: I4531975e10894b46807db7c3fdebf0203ffbca77
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -4532,6 +4532,10 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
             if (newTask != null) newTask.onAppFocusChanged(true);
         }
 
+        if (mDisplayId == DEFAULT_DISPLAY && newFocus != null) {
+            IAxSandboxService.get().onAppFocusChanged(newFocus, newTask);
+        }
+
         if (newFocus != null && isDefaultDisplay) {
             final GameSpaceService gameSpaceService =
                     LocalServices.getService(GameSpaceService.class);
