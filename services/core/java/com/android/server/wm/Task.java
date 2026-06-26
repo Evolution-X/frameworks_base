f3567f71a20ef9c98081c811f8eb73fb20358b9a
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-26 15:11:44 +0800

    core: adding axion sandbox support
    Change-Id: Ia754feadff534a29c08306f57afbc860ea8ef70b
    
    credit
    HMA OSS | Furkan Karcıoğlu
    
    [joeyhuab] Adapt to A17
    
    Co-authored-by: Furkan Karcıoğlu <45714956+frknkrc44@users.noreply.github.com>
    Co-auhtored-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Change-Id: I4531975e10894b46807db7c3fdebf0203ffbca77
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -2346,6 +2346,7 @@ class Task extends TaskFragment {
 
         if (prevWindowingMode != getWindowingMode()) {
             taskDisplayArea.onRootTaskWindowingModeChanged(this);
+            IAxSandboxService.get().onWindowingModeChanged(this, prevWindowingMode);
         }
 
         if (!isOrganized() && !getRequestedOverrideBounds().isEmpty() && mDisplayContent != null) {
