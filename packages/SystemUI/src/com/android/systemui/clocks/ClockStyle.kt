fb1a83ee419beb27d051f32a6d1ce8ad7d1166a7
Ghosuto <clash.raja10@gmail.com>
2026-07-13 18:48:32 +0000

    SystemUI: Add NOS4 clock style [1/2]
    - fonts taken reddit `FarToday8670`
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.kt
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.kt
@@ -837,6 +837,8 @@ class ClockStyle @JvmOverloads constructor(
             R.layout.keyguard_clock_neu2,           // 87
             R.layout.keyguard_clock_neu3,           // 88
             R.layout.keyguard_clock_badeen,         // 89
+            R.layout.keyguard_clock_space_age,      // 90
+            R.layout.keyguard_clock_polyline,       // 91
         )
 
         private val NO_COLOR_CLOCKS = hashSetOf(1, 2, 25, 26)
