faed1a6d6ff5d55274ac1c341bd91bfbdea68083
Elluzion <dyrex2004@gmail.com>
2026-07-14 19:50:31 +0900

    SystemUI: Blur the power menu
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 31 Jan 2023 07:21:15 +0530
        Subject: SystemUI: Also blur power sub-menus
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Dhina17 <dhinalogu@gmail.com>
        Date: Tue, 5 Sep 2023 17:04:49 +0530
        Subject: SystemUI: Enable power menu blur via window flags
    
        Enabling blur behind via style attr causes an unexpected behaviour
        that power menu always has the blur behind even when the device disables the systemui blur.
        [persist.sysui.disableBlur=1]
    
        GlobalActionsDialog is a part of SystemUI so it should follow BlurUtils#supportBlursOnWindows
        not only Settings.Global.DISABLE_WINDOW_BLURS.
    
        With style attr, it only followed Settings.Global.DISABLE_WINDOW_BLURS, that's why worked fine
        with blur toggle in targets which don't disable systemui blur.
    
        Change-Id: If6aa97ce3df7f12e181274d85a05531fcb575ec5
        [@/neobuddy89: Adapted for our code.]
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [@/neobuddy89: Adapted for A16.]
    [@/joeyhuab: Adapted for A17.]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -158,6 +158,7 @@ import com.android.systemui.scrim.ScrimDrawable;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.shade.ShadeController;
 import com.android.systemui.shade.ShadeDisplayAware;
+import com.android.systemui.statusbar.BlurUtils;
 import com.android.systemui.statusbar.VibratorHelper;
 import com.android.systemui.statusbar.phone.DialogDelegate;
 import com.android.systemui.statusbar.phone.LightBarController;
@@ -2969,6 +2970,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         private final GestureDetector mGestureDetector;
         @NonNull
         private final DeviceEntryInteractor mDeviceEntryInteractor;
+        @NonNull
+        private final BlurUtils mBlurUtils;
 
         @VisibleForTesting
         @Nullable
@@ -3060,7 +3063,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 @NonNull AccessibilityManager accessibilityManager,
                 @NonNull DialogTransitionAnimator dialogTransitionAnimator,
                 @NonNull SystemUIDialog.Factory systemUIDialogFactory,
-                @NonNull DeviceEntryInteractor deviceEntryInteractor) {
+                @NonNull DeviceEntryInteractor deviceEntryInteractor,
+                @NonNull BlurUtils blurUtils) {
             mContext = context;
             mAdapter = adapter;
             mOverflowAdapter = overflowAdapter;
@@ -3085,6 +3089,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             mSystemUIDialogFactory = systemUIDialogFactory;
             mGestureDetector = new GestureDetector(context, mGestureListener);
             mDeviceEntryInteractor = deviceEntryInteractor;
+            mBlurUtils = blurUtils;
             mOnBackInvokedCallback = () -> {
                 logOnBackInvocation();
                 if (mAdapter.isShowingRestartOptions()) {
@@ -3169,7 +3174,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         }
 
         public void showPowerOptionsMenu() {
-            mPowerOptionsDialog = GlobalActionsPowerDialog.create(mContext, mPowerOptionsAdapter);
+            mPowerOptionsDialog = GlobalActionsPowerDialog.create(mContext, mPowerOptionsAdapter,
+                    mBlurUtils);
             mPowerOptionsDialog.show();
         }
 
@@ -3179,7 +3185,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         }
 
         public void showUsersMenu() {
-            mUsersDialog = GlobalActionsPowerDialog.create(mContext, mUsersAdapter);
+            mUsersDialog = GlobalActionsPowerDialog.create(mContext, mUsersAdapter, mBlurUtils);
             mUsersDialog.show();
         }
 
@@ -3239,6 +3245,21 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
 
             if (mBackgroundDrawable == null) {
                 mBackgroundDrawable = new ScrimDrawable();
+            }
+
+            Window window = dialog.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+            if (mBlurUtils.supportsBlursOnWindows()) {
+                // Enable blur behind
+                // Enable dim behind since we are setting some amount dim for the blur.
+                window.addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+                // Set blur behind radius
+                int blurBehindRadius = mContext.getResources()
+                        .getDimensionPixelSize(com.android.systemui.res.R.dimen.max_window_blur_radius);
+                window.getAttributes().setBlurBehindRadius(blurBehindRadius);
+                window.setDimAmount(0.54f);
+            } else {
+                window.setDimAmount(0.88f);
             }
             // If user entered from the lock screen and smart lock was enabled, disable it
             int user = mSelectedUserInteractor.getSelectedUserId();
