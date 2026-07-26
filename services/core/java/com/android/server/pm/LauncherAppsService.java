0c8e63d75fb62cc65e9025df5563dcac9608ccee
Saikrishna1504 <saikrishna26918@gmail.com>
2026-07-26 06:35:00 +0000

    sandbox: Implement "Hide from Launcher" option
    Change-Id: I12e7051e07eb7aab6ef0d963dd2e0149a0f5c032
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- services/core/java/com/android/server/pm/LauncherAppsService.java
+++ services/core/java/com/android/server/pm/LauncherAppsService.java
@@ -1051,6 +1051,17 @@ public class LauncherAppsService extends SystemService {
             boolean isCallerSandboxApp = callingPackage != null
                 && callingPackage.contains(AxSandboxService.SANDBOX_PACKAGE);
 
+            List<String> hiddenPkgs = Collections.emptyList();
+            List<String> launcherHiddenPkgs = Collections.emptyList();
+            if (!isCallerSandboxApp) {
+                try {
+                    hiddenPkgs = IAxSandboxService.get().getHiddenPackages();
+                    launcherHiddenPkgs = IAxSandboxService.get().getHiddenFromLauncherPackages();
+                } catch (Exception e) {
+                    Slog.e(TAG, "Failed to retrieve hidden package lists from AxSandboxService", e);
+                }
+            }
+
             for (int i = 0; i < numResolveInfos; i++) {
                 final ResolveInfo ri = apps.get(i);
                 final String packageName = ri.activityInfo.packageName;
@@ -1059,7 +1070,8 @@ public class LauncherAppsService extends SystemService {
                     continue;
                 }
 
-                if (!isCallerSandboxApp && IAxSandboxService.get().isPackageHidden(packageName)) {
+                if (!isCallerSandboxApp && (hiddenPkgs.contains(packageName)
+                        || launcherHiddenPkgs.contains(packageName))) {
                     continue;
                 }
 
