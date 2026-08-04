54f052c5c4615d4e56b0ffa48d6a22518f504753
Joey <joey@evolution-x.org>
2026-08-05 03:38:33 +0900

    PixelPropsUtils: Update fingerprints to August 2026 release

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -158,8 +158,8 @@ public final class PixelPropsUtils {
         propsToChangeRecentPixel.put("PRODUCT", "mustang");
         propsToChangeRecentPixel.put("HARDWARE", "mustang");
         propsToChangeRecentPixel.put("MODEL", "Pixel 10 Pro XL");
-        propsToChangeRecentPixel.put("ID", "CP2A.260705.006");
-        propsToChangeRecentPixel.put("FINGERPRINT", "google/mustang/mustang:17/CP2A.260705.006/15641320:user/release-keys");
+        propsToChangeRecentPixel.put("ID", "CP2A.260805.005");
+        propsToChangeRecentPixel.put("FINGERPRINT", "google/mustang/mustang:17/CP2A.260805.005/15828068:user/release-keys");
         propsToChangePixelTablet = new HashMap<>();
         propsToChangePixelTablet.put("BRAND", "google");
         propsToChangePixelTablet.put("BOARD", "tangorpro");
