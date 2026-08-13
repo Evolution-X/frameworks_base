b5e199a6c5ac6ca1319a217d2ade9111e0ae9a6c
hiroshi. <hiroshi@tanuki.page>
2026-08-12 22:42:42 -0400

    SystemUI: Clone deviceEntryBackgroundViewAlpha for ALTERNATE_BOUNCER -> GONE
    Test: Unlock with udfps after tapping on a notification, then pull down notif shade

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/viewmodel/AlternateBouncerToGoneTransitionViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/viewmodel/AlternateBouncerToGoneTransitionViewModel.kt
@@ -64,6 +64,14 @@ constructor(
         )
     }
 
+    val deviceEntryBackgroundViewAlpha: Flow<Float> =
+    transitionAnimation.sharedFlowWithShade(
+        duration = 300.milliseconds,
+        onStep = { step, isShadeExpanded -> if (isShadeExpanded) 0f else 1f - step },
+        onCancel = { 0f },
+        onFinish = { 0f },
+    )
+
     fun notificationAlpha(viewState: ViewStateAccessor): Flow<Float> {
         var startAlpha = 1f
         var leaveShadeOpen = false
