805976e220217991c1a89b6519e50edd941130ca
FundaAgent <323139829+FundaAgent@users.noreply.github.com>
2026-09-02 16:50:17 +0000

    services: Drop two per-event info logs from system_server
    BackgroundInstallControlService logged "Package event received" for
    every usage event it is told about (35 lines/min on a Pixel 9 in normal
    use); it was added as temporary test-flakiness debugging and carries no
    information. Remove it.
    
    DisplayDeviceRepository logged "Display device changed render timings"
    at info level every time an LTPO panel moved between 60 and 120 Hz
    (50 lines/min). Log it at debug level.
    
    Change-Id: I3f322b6917006ce7dd36ea6b6e6424807b4bd24e
    (cherry picked from commit 43a0166356f67dc549880eb618b214524a25251e)

--- services/core/java/com/android/server/display/DisplayDeviceRepository.java
+++ services/core/java/com/android/server/display/DisplayDeviceRepository.java
@@ -197,7 +197,7 @@ class DisplayDeviceRepository implements DisplayAdapter.Listener {
             } else if (DEBUG && (diff &
                     (DisplayDeviceInfo.DIFF_MODE_ID | DisplayDeviceInfo.DIFF_RENDER_TIMINGS
                             | DisplayDeviceInfo.DIFF_FRAME_RATE_OVERRIDE)) != 0) {
-                Slog.i(TAG, "Display device changed render timings: \"" + info.name
+                Slog.d(TAG, "Display device changed render timings: \"" + info.name
                         + "\", renderFrameRate=" + info.renderFrameRate
                         + ", presentationDeadlineNanos=" + info.presentationDeadlineNanos
                         + ", appVsyncOffsetNanos=" + info.appVsyncOffsetNanos
