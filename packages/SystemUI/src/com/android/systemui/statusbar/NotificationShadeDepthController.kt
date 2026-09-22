9e59915adb085d4c00a0f07af92e433d4f9a9bb7
Ghosuto <clash.raja10@gmail.com>
2026-09-22 07:39:08 +0530

    SystemUI: Reduce redundant shade blur update work
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationShadeDepthController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationShadeDepthController.kt
@@ -681,15 +681,14 @@ constructor(
     }
 
     private fun scheduleUpdate() {
-        val (blur, zoomOutFromShadeRadius) = computeBlurAndZoomOut()
-        zoomOutCalculatedFromShadeRadius = zoomOutFromShadeRadius
-
         // Do not blur or zoom out the wallpaper if the blurred wallpaper is not supported.
         if (!windowRootViewBlurInteractor.isBlurredWallpaperOnShadeSupported) {
             return
         }
 
         if (Flags.bouncerUiRevamp() || Flags.glanceableHubBlurredBackground()) {
+            val (blur, zoomOutFromShadeRadius) = computeBlurAndZoomOut()
+            zoomOutCalculatedFromShadeRadius = zoomOutFromShadeRadius
             if (windowRootViewBlurInteractor.isBlurCurrentlySupported.value) {
                 updateScheduled =
                     windowRootViewBlurInteractor.requestBlurForShade(
@@ -710,6 +709,7 @@ constructor(
             return
         }
         updateScheduled = true
+        val (blur, _) = computeBlurAndZoomOut()
         blurUtils.prepareBlur(blur)
         choreographer.postFrameCallback(updateBlurCallback)
     }
